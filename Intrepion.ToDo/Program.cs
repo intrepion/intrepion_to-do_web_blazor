d0e2b84a51f6f7839fe79d226a5406582e7e34d8
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-22 20:13:19 -0600

    dotnet format

--- Intrepion.ToDo/Program.cs
+++ Intrepion.ToDo/Program.cs
@@ -1,10 +1,10 @@
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Intrepion.ToDo.Client.Pages;
+﻿using Intrepion.ToDo.Client.Pages;
 using Intrepion.ToDo.Components;
 using Intrepion.ToDo.Components.Account;
 using Intrepion.ToDo.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
