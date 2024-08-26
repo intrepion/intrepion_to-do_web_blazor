48cc91a00902d7ac68d8f3ba0f22b33f019aed95
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-26 11:15:33 -0600

    cp -r ../intrepion-bash/dotnet/files/AppNamePlaceholder/Controllers Intrepion.ToDo/

---
+++ Intrepion.ToDo/Controllers/ApplicationRoleController.cs
@@ -0,0 +1,44 @@
+using AppNamePlaceholder.Shared.Entities;
+using AppNamePlaceholder.Shared.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppNamePlaceholder.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ApplicationRoleController(IApplicationRoleService applicationRoleService) : ControllerBase
+{
+    private readonly IApplicationRoleService _applicationRoleService = applicationRoleService;
+
+    [HttpPost]
+    public async Task<ActionResult<ApplicationRole>> Add(ApplicationRole applicationRole)
+    {
+        var addedApplicationRole = await _applicationRoleService.AddAsync(applicationRole);
+
+        return Ok(addedApplicationRole);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ApplicationRole>> Delete(Guid id)
+    {
+        var result = await _applicationRoleService.DeleteAsync(id);
+
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ApplicationRole>> Edit(Guid id, ApplicationRole applicationRole)
+    {
+        var updatedApplicationRole = await _applicationRoleService.EditAsync(id, applicationRole);
+
+        return Ok(updatedApplicationRole);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ApplicationRole>> GetById(Guid id)
+    {
+        var applicationRole = await _applicationRoleService.GetByIdAsync(id);
+
+        return Ok(applicationRole);
+    }
+}
