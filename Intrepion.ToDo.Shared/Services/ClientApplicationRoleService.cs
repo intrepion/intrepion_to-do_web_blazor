16802668743135c37a31541a8953fc1c819a3193
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-26 11:15:41 -0600

    cp ../intrepion-bash/dotnet/files/AppNamePlaceholder.Shared/Services/ClientApplicationRoleService.cs Intrepion.ToDo.Shared/Services/

---
+++ Intrepion.ToDo.Shared/Services/ClientApplicationRoleService.cs
@@ -0,0 +1,44 @@
+using System.Net.Http.Json;
+using AppNamePlaceholder.Shared.Entities;
+
+namespace AppNamePlaceholder.Shared.Services;
+
+public class ClientApplicationRoleService(HttpClient httpClient) : IApplicationRoleService
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+    public async Task<ApplicationRole> AddAsync(ApplicationRole applicationRole)
+    {
+        var result = await _httpClient
+            .PostAsJsonAsync("/api/applicationRole", applicationRole);
+
+        return await result.Content.ReadFromJsonAsync<ApplicationRole>();
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var result = await _httpClient.DeleteAsync($"/api/applicationRole/{id}");
+
+        return await result.Content.ReadFromJsonAsync<bool>();
+    }
+
+    public async Task<ApplicationRole> EditAsync(Guid id, ApplicationRole applicationRole)
+    {
+        var result = await _httpClient.PutAsJsonAsync($"/api/applicationRole/{id}", applicationRole);
+
+        return await result.Content.ReadFromJsonAsync<ApplicationRole>();
+    }
+
+    public Task<List<ApplicationRole>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ApplicationRole> GetByIdAsync(Guid id)
+    {
+        var result = await _httpClient
+            .GetFromJsonAsync<ApplicationRole>($"/api/applicationRole/{id}");
+
+        return result;
+    }
+}
