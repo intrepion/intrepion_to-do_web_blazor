1bb8eff2bb92c632f2090fbe649d65d078e09cf4
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-26 11:15:39 -0600

    cp ../intrepion-bash/dotnet/files/AppNamePlaceholder.Shared/Entities/ApplicationUser.cs Intrepion.ToDo.Shared/Entities/

---
+++ Intrepion.ToDo.Shared/Entities/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AppNamePlaceholder.Shared.Entities;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
