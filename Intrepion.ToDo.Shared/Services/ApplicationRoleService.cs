5dc076e393e1b328f0d697f7c584af741d477b1a
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-26 11:15:40 -0600

    cp ../intrepion-bash/dotnet/files/AppNamePlaceholder.Shared/Services/ApplicationRoleService.cs Intrepion.ToDo.Shared/Services/

---
+++ Intrepion.ToDo.Shared/Services/ApplicationRoleService.cs
@@ -0,0 +1,62 @@
+﻿using AppNamePlaceholder.Shared.Data;
+using AppNamePlaceholder.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppNamePlaceholder.Shared.Services;
+
+public class ApplicationRoleService(ApplicationDbContext applicationDbContext) : IApplicationRoleService
+{
+    private readonly ApplicationDbContext _applicationDbContext = applicationDbContext;
+
+    public async Task<ApplicationRole> AddAsync(ApplicationRole applicationRole)
+    {
+        _applicationDbContext.Roles.Add(applicationRole);
+        await _applicationDbContext.SaveChangesAsync();
+
+        return applicationRole;
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var dbApplicationRole = await _applicationDbContext.Roles.FindAsync(id);
+
+        if (dbApplicationRole == null)
+        {
+            return false;
+        }
+
+        _applicationDbContext.Remove(dbApplicationRole);
+        await _applicationDbContext.SaveChangesAsync();
+
+        return true;
+    }
+
+    public async Task<ApplicationRole> EditAsync(Guid id, ApplicationRole applicationRole)
+    {
+        var dbApplicationRole = await _applicationDbContext.Roles.FindAsync(id);
+
+        if (dbApplicationRole == null)
+        {
+            throw new Exception("ApplicationRole not found.");
+        }
+
+        dbApplicationRole.Name = applicationRole.Name;
+        await _applicationDbContext.SaveChangesAsync();
+
+        return dbApplicationRole;
+    }
+
+    public async Task<List<ApplicationRole>> GetAllAsync()
+    {
+        await Task.Delay(1000);
+
+        var applicationRoles = await _applicationDbContext.Roles.ToListAsync();
+
+        return applicationRoles;
+    }
+
+    public async Task<ApplicationRole> GetByIdAsync(Guid id)
+    {
+        return await _applicationDbContext.Roles.FindAsync(id);
+    }
+}
