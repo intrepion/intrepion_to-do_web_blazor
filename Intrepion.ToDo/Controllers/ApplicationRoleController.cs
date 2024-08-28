78358a14201e132184222adf9b217f88e8e89ead
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-28 16:02:17 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/AppNamePlaceholder/* Intrepion.ToDo/

---
+++ Intrepion.ToDo/Controllers/ApplicationRoleController.cs
@@ -0,0 +1,60 @@
+﻿using AppNamePlaceholder.Shared.Entities;
+using AppNamePlaceholder.Shared.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppNamePlaceholder.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ApplicationRoleController(IApplicationRoleService applicationRoleService) : ControllerBase
+{
+    private readonly IApplicationRoleService _applicationRoleService = applicationRoleService;
+
+    [HttpPost]
+    public async Task<ActionResult<ApplicationRole>> Add(ApplicationRole applicationRole)
+    {
+        var userName = User.Identity?.Name;
+
+        var addedApplicationRole = await _applicationRoleService.AddAsync(userName, applicationRole);
+
+        return Ok(addedApplicationRole);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ApplicationRole>> Delete(string id)
+    {
+        var userName = User.Identity?.Name;
+
+        var result = await _applicationRoleService.DeleteAsync(userName, id);
+
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ApplicationRole>> Edit(string id, ApplicationRole applicationRole)
+    {
+        var userName = User.Identity?.Name;
+
+        var updatedApplicationRole = await _applicationRoleService.EditAsync(userName, id, applicationRole);
+
+        return Ok(updatedApplicationRole);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ApplicationRole>> GetAll()
+    {
+        var userName = User.Identity?.Name;
+
+        var applicationRoles = await _applicationRoleService.GetAllAsync();
+
+        return Ok(applicationRoles);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ApplicationRole>> GetById(string id)
+    {
+        var applicationRole = await _applicationRoleService.GetByIdAsync(id);
+
+        return Ok(applicationRole);
+    }
+}
---
+++ Intrepion.ToDo/Controllers/ApplicationUserController.cs
@@ -0,0 +1,58 @@
+﻿using AppNamePlaceholder.Shared.Entities;
+using AppNamePlaceholder.Shared.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppNamePlaceholder.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ApplicationUserController(IApplicationUserService applicationUserService) : ControllerBase
+{
+    private readonly IApplicationUserService _applicationUserService = applicationUserService;
+
+    [HttpPost]
+    public async Task<ActionResult<ApplicationUser>> Add(ApplicationUser applicationUser)
+    {
+        var userName = User.Identity?.Name;
+
+        var addedApplicationUser = await _applicationUserService.AddAsync(userName, applicationUser);
+
+        return Ok(addedApplicationUser);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ApplicationUser>> Delete(string id)
+    {
+        var userName = User.Identity?.Name;
+
+        var result = await _applicationUserService.DeleteAsync(userName, id);
+
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ApplicationUser>> Edit(string id, ApplicationUser applicationUser)
+    {
+        var userName = User.Identity?.Name;
+
+        var updatedApplicationUser = await _applicationUserService.EditAsync(userName, id, applicationUser);
+
+        return Ok(updatedApplicationUser);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ApplicationUser>> GetAll()
+    {
+        var applicationUsers = await _applicationUserService.GetAllAsync();
+
+        return Ok(applicationUsers);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ApplicationUser>> GetById(string id)
+    {
+        var applicationUser = await _applicationUserService.GetByIdAsync(id);
+
+        return Ok(applicationUser);
+    }
+}
