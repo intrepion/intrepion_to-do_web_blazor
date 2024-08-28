3948205d7d4c31fa5ad63c59ee5df0388908b747
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-27 23:40:19 -0600

    cp -r ../intrepion-bash/dotnet/files/AppNamePlaceholder/Controllers Intrepion.ToDo/

---
+++ Intrepion.ToDo/Controllers/ApplicationRoleController.cs
@@ -0,0 +1,52 @@
+﻿using AppNamePlaceholder.Shared.Entities;
+using AppNamePlaceholder.Shared.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppNamePlaceholder.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ApplicationRoleController(IApplicationRoleService applicationRoleService) : ControllerBase
+{
+    private readonly IApplicationRoleService _applicationRoleService = applicationRoleService;
+
+    [HttpPost]
+    public async Task<ActionResult<ApplicationRole>> Add(ApplicationRole applicationRole)
+    {
+        var addedGame = await _applicationRoleService.AddAsync(applicationRole);
+
+        return Ok(addedGame);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ApplicationRole>> Delete(string id)
+    {
+        var result = await _applicationRoleService.DeleteAsync(id);
+
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ApplicationRole>> Edit(string id, ApplicationRole applicationRole)
+    {
+        var updatedGame = await _applicationRoleService.EditAsync(id, applicationRole);
+
+        return Ok(updatedGame);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ApplicationRole>> GetAll()
+    {
+        var games = await _applicationRoleService.GetAllAsync();
+
+        return Ok(games);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ApplicationRole>> GetById(string id)
+    {
+        var game = await _applicationRoleService.GetByIdAsync(id);
+
+        return Ok(game);
+    }
+}
---
+++ Intrepion.ToDo/Controllers/ApplicationUserController.cs
@@ -0,0 +1,52 @@
+﻿using AppNamePlaceholder.Shared.Entities;
+using AppNamePlaceholder.Shared.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppNamePlaceholder.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ApplicationUserController(IApplicationUserService applicationUserService) : ControllerBase
+{
+    private readonly IApplicationUserService _applicationUserService = applicationUserService;
+
+    [HttpPost]
+    public async Task<ActionResult<ApplicationUser>> Add(ApplicationUser applicationUser)
+    {
+        var addedGame = await _applicationUserService.AddAsync(applicationUser);
+
+        return Ok(addedGame);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ApplicationUser>> Delete(string id)
+    {
+        var result = await _applicationUserService.DeleteAsync(id);
+
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ApplicationUser>> Edit(string id, ApplicationUser applicationUser)
+    {
+        var updatedGame = await _applicationUserService.EditAsync(id, applicationUser);
+
+        return Ok(updatedGame);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ApplicationUser>> GetAll()
+    {
+        var games = await _applicationUserService.GetAllAsync();
+
+        return Ok(games);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ApplicationUser>> GetById(string id)
+    {
+        var game = await _applicationUserService.GetByIdAsync(id);
+
+        return Ok(game);
+    }
+}
