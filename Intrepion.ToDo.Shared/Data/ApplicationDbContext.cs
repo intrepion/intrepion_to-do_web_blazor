c9974679cfd06c6bd18b6d9ac45da66c7f013f8d
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-27 23:40:24 -0600

    cp -r ../intrepion-bash/dotnet/files/AppNamePlaceholder.Shared/Data Intrepion.ToDo.Shared/

---
+++ Intrepion.ToDo.Shared/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AppNamePlaceholder.Shared.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppNamePlaceholder.Shared.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole, string>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        new ApplicationRoleEntityTypeConfiguration().Configure(builder.Entity<ApplicationRole>());
+        new ApplicationUserEntityTypeConfiguration().Configure(builder.Entity<ApplicationUser>());
+    }
+}
