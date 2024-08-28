2d19cf5ac5ddbcf06c40f58ea4b45b9b241fad14
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-27 23:40:23 -0600

    cp ../intrepion-bash/dotnet/files/AppNamePlaceholder.Client/Program.cs Intrepion.ToDo.Client/

--- Intrepion.ToDo.Client/Program.cs
+++ Intrepion.ToDo.Client/Program.cs
@@ -1,4 +1,6 @@
-﻿using Intrepion.ToDo.Client;
+﻿using AppNamePlaceholder.Client;
+using AppNamePlaceholder.Shared.Services.Client;
+using AppNamePlaceholder.Shared.Services.Interfaces;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+builder.Services.AddScoped(http => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+});
+
+builder.Services.AddScoped<IApplicationRoleService, ClientApplicationRoleService>();
+builder.Services.AddScoped<IApplicationUserService, ClientApplicationUserService>();
 
 await builder.Build().RunAsync();
