627f003aa7fbc47ce3d8917c1580bfbe016ee61a
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-31 17:53:21 -0600

    Creating an interface

---
+++ Intrepion.ToDo.BusinessLogic/Services/IBlogService.cs
@@ -0,0 +1,23 @@
+﻿using Intrepion.ToDo.BusinessLogic.Entities;
+
+namespace Intrepion.ToDo.BusinessLogic.Services;
+
+public interface IBlogService
+{
+    Task<BlogPost?> GetBlogPostAsync(Guid id);
+    Task<int> GetBlogPostCountAsync();
+    Task<List<BlogPost>> GetBlogPostsAsync(int numberOfPosts, int startIndex);
+    Task<List<Category>> GetCategoriesAsync();
+    Task<Category?> GetCategoryAsync(Guid id);
+    Task<List<Comment>> GetCommentsAsync(Guid blogPostId);
+    Task<Tag?> GetTagAsync(Guid id);
+    Task<List<Tag>> GetTagsAsync();
+    Task<BlogPost?> SaveBlogPostAsync(BlogPost blogPost);
+    Task<Category?> SaveCategoryAsync(Category category);
+    Task<Comment?> SaveCommentAsync(Comment comment);
+    Task<Tag?> SaveTagAsync(Tag tag);
+    Task DeleteBlogPostAsync(Guid id);
+    Task DeleteCategoryAsync(Guid id);
+    Task DeleteCommentAsync(Guid id);
+    Task DeleteTagAsync(Guid id);
+}
