f666d0fed660e15cf473b2a2856f69a67180286a
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-02 14:43:53 -0600

    gawk -i inplace "{ gsub(/AppNamePlaceholder/, \"Intrepion.ToDo\") }; { print }" Intrepion.ToDo.Client/*

--- Intrepion.ToDo.Client/Program.cs
+++ Intrepion.ToDo.Client/Program.cs
@@ -1,6 +1,6 @@
-﻿using AppNamePlaceholder.BusinessLogic.Services;
-using AppNamePlaceholder.BusinessLogic.Services.Client;
-using AppNamePlaceholder.Client;
+﻿using Intrepion.ToDo.BusinessLogic.Services;
+using Intrepion.ToDo.BusinessLogic.Services.Client;
+using Intrepion.ToDo.Client;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
