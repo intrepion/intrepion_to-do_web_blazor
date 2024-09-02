42ca648259c6db62a4a6af8f7312c34622a90056
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-02 14:43:48 -0600

    gawk -i inplace "{ gsub(/AppNamePlaceholder/, \"Intrepion.ToDo\") }; { print }" Intrepion.ToDo.AcceptanceTests/*

--- Intrepion.ToDo.AcceptanceTests/UnitTest1.cs
+++ Intrepion.ToDo.AcceptanceTests/UnitTest1.cs
@@ -2,7 +2,7 @@
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
-namespace AppNamePlaceholder.AcceptanceTests;
+namespace Intrepion.ToDo.AcceptanceTests;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
