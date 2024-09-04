ef36cdf798d48f4022ebdd477ef8b39f9d12c2c7
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-04 00:53:42 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/AppNamePlaceholder/* Intrepion.ToDo/

---
+++ Intrepion.ToDo/Controllers/ApplicationUserController.cs
@@ -0,0 +1,87 @@
+﻿using AppNamePlaceholder.BusinessLogic.Entities;
+using AppNamePlaceholder.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppNamePlaceholder.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ApplicationUserController(IAdminApplicationUserService adminApplicationUserService) : ControllerBase
+{
+    private readonly IAdminApplicationUserService _adminApplicationUserService = adminApplicationUserService;
+
+    [HttpPost]
+    public async Task<ActionResult<ApplicationUser?>> Add(ApplicationUser applicationUser)
+    {
+        var userName = User.Identity?.Name;
+
+        if (userName == null)
+        {
+            return Ok(null);
+        }
+
+        var addedApplicationUser = await _adminApplicationUserService.AddAsync(userName, applicationUser);
+
+        return Ok(addedApplicationUser);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ApplicationUser?>> Delete(string id)
+    {
+        var userName = User.Identity?.Name;
+
+        if (userName == null)
+        {
+            return Ok(null);
+        }
+
+        var result = await _adminApplicationUserService.DeleteAsync(userName, id);
+
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ApplicationUser?>> Edit(string id, ApplicationUser applicationUser)
+    {
+        var userName = User.Identity?.Name;
+
+        if (userName == null)
+        {
+            return Ok(null);
+        }
+
+        var updatedApplicationUser = await _adminApplicationUserService.EditAsync(userName, id, applicationUser);
+
+        return Ok(updatedApplicationUser);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<ApplicationUser>?>> GetAll()
+    {
+        var userName = User.Identity?.Name;
+
+        if (userName == null)
+        {
+            return Ok(null);
+        }
+
+        var applicationUsers = await _adminApplicationUserService.GetAllAsync();
+
+        return Ok(applicationUsers);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ApplicationUser?>> GetById(string id)
+    {
+        var userName = User.Identity?.Name;
+
+        if (userName == null)
+        {
+            return Ok(null);
+        }
+
+        var applicationUser = await _adminApplicationUserService.GetByIdAsync(id);
+
+        return Ok(applicationUser);
+    }
+}
