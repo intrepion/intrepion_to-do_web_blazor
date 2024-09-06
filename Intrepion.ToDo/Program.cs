cea257a285e8263f28626ae383977f41fe5f2f3f
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-06 00:29:09 -0600

    gawk -i inplace "{ gsub(/\/\/ RegisterServerServiceCodePlaceholder/, \"builder.Services.AddScoped<IToDoListAdminService, ToDoListAdminService>();\n// RegisterServerServiceCodePlaceholder\") }; { print }" Intrepion.ToDo/Program.cs

--- Intrepion.ToDo/Program.cs
+++ Intrepion.ToDo/Program.cs
@@ -50,6 +50,7 @@ builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSe
 builder.Services.AddScoped<IAdminApplicationRoleService, AdminApplicationRoleService>();
 builder.Services.AddScoped<IAdminApplicationUserService, AdminApplicationUserService>();
 builder.Services.AddScoped<IToDoItemAdminService, ToDoItemAdminService>();
+builder.Services.AddScoped<IToDoListAdminService, ToDoListAdminService>();
 // RegisterServerServiceCodePlaceholder
 
 var app = builder.Build();
