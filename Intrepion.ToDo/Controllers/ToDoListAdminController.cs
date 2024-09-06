1c6833c4ee9baff95c63d5bab0586924da1f2123
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-06 00:29:14 -0600

    gawk -i inplace "{ gsub(/EntityLowercaseNamePlaceholder/, \"toDoList\") }; { print }" Intrepion.ToDo/Controllers/ToDoListAdminController.cs

--- Intrepion.ToDo/Controllers/ToDoListAdminController.cs
+++ Intrepion.ToDo/Controllers/ToDoListAdminController.cs
@@ -6,12 +6,12 @@ namespace ApplicationNamePlaceholder.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ToDoListAdminController(IToDoListAdminService EntityLowercaseNamePlaceholderAdminService) : ControllerBase
+public class ToDoListAdminController(IToDoListAdminService toDoListAdminService) : ControllerBase
 {
-    private readonly IToDoListAdminService _EntityLowercaseNamePlaceholderAdminService = EntityLowercaseNamePlaceholderAdminService;
+    private readonly IToDoListAdminService _toDoListAdminService = toDoListAdminService;
 
     [HttpPost]
-    public async Task<ActionResult<ToDoList?>> Add(ToDoList EntityLowercaseNamePlaceholder)
+    public async Task<ActionResult<ToDoList?>> Add(ToDoList toDoList)
     {
         var userName = User.Identity?.Name;
 
@@ -20,7 +20,7 @@ public class ToDoListAdminController(IToDoListAdminService EntityLowercaseNamePl
             return Ok(null);
         }
 
-        var addedToDoList = await _EntityLowercaseNamePlaceholderAdminService.AddAsync(userName, EntityLowercaseNamePlaceholder);
+        var addedToDoList = await _toDoListAdminService.AddAsync(userName, toDoList);
 
         return Ok(addedToDoList);
     }
@@ -35,13 +35,13 @@ public class ToDoListAdminController(IToDoListAdminService EntityLowercaseNamePl
             return Ok(null);
         }
 
-        var result = await _EntityLowercaseNamePlaceholderAdminService.DeleteAsync(userName, id);
+        var result = await _toDoListAdminService.DeleteAsync(userName, id);
 
         return Ok(result);
     }
 
     [HttpPut("{id}")]
-    public async Task<ActionResult<ToDoList?>> Edit(Guid id, ToDoList EntityLowercaseNamePlaceholder)
+    public async Task<ActionResult<ToDoList?>> Edit(Guid id, ToDoList toDoList)
     {
         var userName = User.Identity?.Name;
 
@@ -50,7 +50,7 @@ public class ToDoListAdminController(IToDoListAdminService EntityLowercaseNamePl
             return Ok(null);
         }
 
-        var updatedToDoList = await _EntityLowercaseNamePlaceholderAdminService.EditAsync(userName, id, EntityLowercaseNamePlaceholder);
+        var updatedToDoList = await _toDoListAdminService.EditAsync(userName, id, toDoList);
 
         return Ok(updatedToDoList);
     }
@@ -65,7 +65,7 @@ public class ToDoListAdminController(IToDoListAdminService EntityLowercaseNamePl
             return Ok(null);
         }
 
-        var toDoLists = await _EntityLowercaseNamePlaceholderAdminService.GetAllAsync();
+        var toDoLists = await _toDoListAdminService.GetAllAsync();
 
         return Ok(toDoLists);
     }
@@ -80,8 +80,8 @@ public class ToDoListAdminController(IToDoListAdminService EntityLowercaseNamePl
             return Ok(null);
         }
 
-        var EntityLowercaseNamePlaceholder = await _EntityLowercaseNamePlaceholderAdminService.GetByIdAsync(id);
+        var toDoList = await _toDoListAdminService.GetByIdAsync(id);
 
-        return Ok(EntityLowercaseNamePlaceholder);
+        return Ok(toDoList);
     }
 }
