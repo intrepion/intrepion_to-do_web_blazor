a79e02bdc149ddf3008520e982d34976833bacf6
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-06 22:46:10 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/EntityNamePlaceholder.cs Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs

---
+++ Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
+
+public class EntityNamePlaceholder
+{
+    public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
+    public Guid Id { get; set; }
+    // ActualPropertyPlaceholder
+}
