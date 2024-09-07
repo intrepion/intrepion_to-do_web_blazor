56a28e5ff19d0d6cef762c864a24bf29c9320907
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-06 22:46:22 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"ToDoList\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/ToDoList.cs

--- Intrepion.ToDo.BusinessLogic/Entities/ToDoList.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/ToDoList.cs
@@ -1,6 +1,6 @@
 ﻿namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
 
-public class EntityNamePlaceholder
+public class ToDoList
 {
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
