611b160ce38152b0468f2e48c0c6e6155cf01028
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-11 23:07:41 -0600

    gawk -i inplace "{ gsub(/EntityLowercaseNamePlaceholder/, \"toDoList\") }; { print }" Intrepion.ToDo.BusinessLogic/Services/IToDoListAdminService.cs

--- Intrepion.ToDo.BusinessLogic/Services/IToDoListAdminService.cs
+++ Intrepion.ToDo.BusinessLogic/Services/IToDoListAdminService.cs
@@ -4,9 +4,9 @@ namespace ApplicationNamePlaceholder.BusinessLogic.Services;
 
 public interface IToDoListAdminService
 {
-    Task<ToDoList?> AddAsync(string userName, ToDoList EntityLowercaseNamePlaceholder);
+    Task<ToDoList?> AddAsync(string userName, ToDoList toDoList);
     Task<bool> DeleteAsync(string userName, Guid id);
-    Task<ToDoList?> EditAsync(string userName, Guid id, ToDoList EntityLowercaseNamePlaceholder);
+    Task<ToDoList?> EditAsync(string userName, Guid id, ToDoList toDoList);
     Task<List<ToDoList>?> GetAllAsync();
     Task<ToDoList?> GetByIdAsync(Guid id);
 }
