a036dc888e9a4b978003b623828ed809f4ae6a62
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-19 22:15:10 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/Models/EntityNamePlaceholderAdminEditModel.cs Intrepion.ToDo.BusinessLogic/Entities/Models/ToDoListAdminEditModel.cs

---
+++ Intrepion.ToDo.BusinessLogic/Entities/Models/ToDoListAdminEditModel.cs
@@ -0,0 +1,40 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities.DataTransferObjects;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Models;
+
+public class EntityNamePlaceholderAdminEditModel
+{
+    public Guid Id { get; set; }
+
+    // ModelPropertyPlaceholder
+
+    public static EntityNamePlaceholderAdminEditModel FromEntityNamePlaceholderAdminDataTransferObject(EntityNamePlaceholderAdminDataTransferObject? EntityLowercaseNamePlaceholderAdminDataTransferObject)
+    {
+        if (EntityLowercaseNamePlaceholderAdminDataTransferObject == null)
+        {
+            return new EntityNamePlaceholderAdminEditModel();
+        }
+
+        return new EntityNamePlaceholderAdminEditModel
+        {
+            Id = EntityLowercaseNamePlaceholderAdminDataTransferObject.Id,
+
+            // DtoToModelPropertyPlaceholder
+        };
+    }
+
+    public static EntityNamePlaceholderAdminDataTransferObject ToEntityNamePlaceholderAdminDataTransferObject(EntityNamePlaceholderAdminEditModel? EntityLowercaseNamePlaceholderAdminEditModel)
+    {
+        if (EntityLowercaseNamePlaceholderAdminEditModel == null)
+        {
+            return new EntityNamePlaceholderAdminDataTransferObject();
+        }
+
+        return new EntityNamePlaceholderAdminDataTransferObject
+        {
+            Id = EntityLowercaseNamePlaceholderAdminEditModel.Id,
+
+            // ModelToDtoPropertyPlaceholder
+        };
+    }
+}
