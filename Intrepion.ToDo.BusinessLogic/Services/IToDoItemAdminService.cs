c535a941f05f0427e6cd2f6e71ffc1709ad520c2
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-19 22:14:45 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"ToDoItem\") }; { print }" Intrepion.ToDo.BusinessLogic/Services/IToDoItemAdminService.cs

--- Intrepion.ToDo.BusinessLogic/Services/IToDoItemAdminService.cs
+++ Intrepion.ToDo.BusinessLogic/Services/IToDoItemAdminService.cs
@@ -2,11 +2,11 @@
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Services;
 
-public interface IEntityNamePlaceholderAdminService
+public interface IToDoItemAdminService
 {
-    Task<EntityNamePlaceholderAdminDataTransferObject?> AddAsync(string userName, EntityNamePlaceholderAdminDataTransferObject toDoItem);
+    Task<ToDoItemAdminDataTransferObject?> AddAsync(string userName, ToDoItemAdminDataTransferObject toDoItem);
     Task<bool> DeleteAsync(string userName, Guid id);
-    Task<EntityNamePlaceholderAdminDataTransferObject?> EditAsync(string userName, Guid id, EntityNamePlaceholderAdminDataTransferObject toDoItem);
-    Task<List<EntityNamePlaceholderAdminDataTransferObject>?> GetAllAsync();
-    Task<EntityNamePlaceholderAdminDataTransferObject?> GetByIdAsync(Guid id);
+    Task<ToDoItemAdminDataTransferObject?> EditAsync(string userName, Guid id, ToDoItemAdminDataTransferObject toDoItem);
+    Task<List<ToDoItemAdminDataTransferObject>?> GetAllAsync();
+    Task<ToDoItemAdminDataTransferObject?> GetByIdAsync(Guid id);
 }
