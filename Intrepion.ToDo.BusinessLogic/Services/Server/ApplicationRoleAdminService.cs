64260763516208f7cfe21e6bccfbfede61f01a8d
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-19 22:15:59 -0600

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.ToDo\") }; { print }" Intrepion.ToDo.BusinessLogic/Services/Server/*

--- Intrepion.ToDo.BusinessLogic/Services/Server/ApplicationRoleAdminService.cs
+++ Intrepion.ToDo.BusinessLogic/Services/Server/ApplicationRoleAdminService.cs
@@ -1,9 +1,9 @@
-﻿using ApplicationNamePlaceholder.BusinessLogic.Data;
-using ApplicationNamePlaceholder.BusinessLogic.Entities;
-using ApplicationNamePlaceholder.BusinessLogic.Entities.DataTransferObjects;
+﻿using Intrepion.ToDo.BusinessLogic.Data;
+using Intrepion.ToDo.BusinessLogic.Entities;
+using Intrepion.ToDo.BusinessLogic.Entities.DataTransferObjects;
 using Microsoft.EntityFrameworkCore;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Services.Server;
+namespace Intrepion.ToDo.BusinessLogic.Services.Server;
 
 public class ApplicationRoleAdminService(ApplicationDbContext applicationDbContext) : IApplicationRoleAdminService
 {
