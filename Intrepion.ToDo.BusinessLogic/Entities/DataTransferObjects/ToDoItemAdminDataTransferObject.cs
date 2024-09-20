32caa04bff57742c721c1b4221e5c254b328ce73
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-19 22:14:41 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/DataTransferObjects/EntityNamePlaceholderAdminDataTransferObject.cs Intrepion.ToDo.BusinessLogic/Entities/DataTransferObjects/ToDoItemAdminDataTransferObject.cs

---
+++ Intrepion.ToDo.BusinessLogic/Entities/DataTransferObjects/ToDoItemAdminDataTransferObject.cs
@@ -0,0 +1,34 @@
+﻿namespace ApplicationNamePlaceholder.BusinessLogic.Entities.DataTransferObjects;
+
+public class EntityNamePlaceholderAdminDataTransferObject
+{
+    public Guid Id { get; set; }
+
+    // DtoPropertyPlaceholder
+
+    public static EntityNamePlaceholderAdminDataTransferObject FromEntityNamePlaceholder(EntityNamePlaceholder? EntityLowercaseNamePlaceholder)
+    {
+        if (EntityLowercaseNamePlaceholder == null)
+        {
+            return new EntityNamePlaceholderAdminDataTransferObject();
+        }
+
+        return new EntityNamePlaceholderAdminDataTransferObject
+        {
+            Id = EntityLowercaseNamePlaceholder.Id,
+
+            // EntityToDtoPropertyPlaceholder
+        };
+    }
+
+    public static EntityNamePlaceholder ToEntityNamePlaceholder(ApplicationUser applicationUser, EntityNamePlaceholderAdminDataTransferObject EntityLowercaseNamePlaceholderAdminDataTransferObject)
+    {
+        return new EntityNamePlaceholder
+        {
+            ApplicationUserUpdatedBy = applicationUser,
+            Id = EntityLowercaseNamePlaceholderAdminDataTransferObject.Id,
+
+            // DtoToEntityPropertyPlaceholder
+        };
+    }
+}
