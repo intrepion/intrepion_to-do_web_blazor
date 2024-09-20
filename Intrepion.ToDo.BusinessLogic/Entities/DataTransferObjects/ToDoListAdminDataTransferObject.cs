0b890a0de87210150f0bf931c4afbb913d8cf268
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-19 22:15:10 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"ToDoList\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/DataTransferObjects/ToDoListAdminDataTransferObject.cs

--- Intrepion.ToDo.BusinessLogic/Entities/DataTransferObjects/ToDoListAdminDataTransferObject.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/DataTransferObjects/ToDoListAdminDataTransferObject.cs
@@ -1,19 +1,19 @@
 ﻿namespace ApplicationNamePlaceholder.BusinessLogic.Entities.DataTransferObjects;
 
-public class EntityNamePlaceholderAdminDataTransferObject
+public class ToDoListAdminDataTransferObject
 {
     public Guid Id { get; set; }
 
     // DtoPropertyPlaceholder
 
-    public static EntityNamePlaceholderAdminDataTransferObject FromEntityNamePlaceholder(EntityNamePlaceholder? toDoList)
+    public static ToDoListAdminDataTransferObject FromToDoList(ToDoList? toDoList)
     {
         if (toDoList == null)
         {
-            return new EntityNamePlaceholderAdminDataTransferObject();
+            return new ToDoListAdminDataTransferObject();
         }
 
-        return new EntityNamePlaceholderAdminDataTransferObject
+        return new ToDoListAdminDataTransferObject
         {
             Id = toDoList.Id,
 
@@ -21,9 +21,9 @@ public class EntityNamePlaceholderAdminDataTransferObject
         };
     }
 
-    public static EntityNamePlaceholder ToEntityNamePlaceholder(ApplicationUser applicationUser, EntityNamePlaceholderAdminDataTransferObject toDoListAdminDataTransferObject)
+    public static ToDoList ToToDoList(ApplicationUser applicationUser, ToDoListAdminDataTransferObject toDoListAdminDataTransferObject)
     {
-        return new EntityNamePlaceholder
+        return new ToDoList
         {
             ApplicationUserUpdatedBy = applicationUser,
             Id = toDoListAdminDataTransferObject.Id,
