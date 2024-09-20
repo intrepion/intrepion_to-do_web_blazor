b8cd6fd05c80c120bbd6331842754d725d645245
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-19 22:14:34 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder/* Intrepion.ToDo/

---
+++ Intrepion.ToDo/Controllers/ApplicationUserAdminController.cs
@@ -0,0 +1,88 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities;
+using ApplicationNamePlaceholder.BusinessLogic.Entities.DataTransferObjects;
+using ApplicationNamePlaceholder.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationNamePlaceholder.Controllers;
+
+[Route("api/admin/[controller]")]
+[ApiController]
+public class ApplicationUserController(IApplicationUserAdminService adminApplicationUserService) : ControllerBase
+{
+    private readonly IApplicationUserAdminService _adminApplicationUserService = adminApplicationUserService;
+
+    [HttpPost]
+    public async Task<ActionResult<ApplicationUserAdminDataTransferObject?>> Add(ApplicationUserAdminDataTransferObject applicationUserAdminDataTransferObject)
+    {
+        var userName = User.Identity?.Name;
+
+        if (userName == null)
+        {
+            return Ok(null);
+        }
+
+        var databaseApplicationUserAdminDataTransferObject = await _adminApplicationUserService.AddAsync(userName, applicationUserAdminDataTransferObject);
+
+        return Ok(databaseApplicationUserAdminDataTransferObject);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ApplicationUser?>> Delete(Guid id)
+    {
+        var userName = User.Identity?.Name;
+
+        if (userName == null)
+        {
+            return Ok(null);
+        }
+
+        var result = await _adminApplicationUserService.DeleteAsync(userName, id);
+
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ApplicationUserAdminDataTransferObject?>> Edit(Guid id, ApplicationUserAdminDataTransferObject applicationUserAdminDataTransferObject)
+    {
+        var userName = User.Identity?.Name;
+
+        if (userName == null)
+        {
+            return Ok(null);
+        }
+
+        var databaseApplicationUserAdminDataTransferObject = await _adminApplicationUserService.EditAsync(userName, id, applicationUserAdminDataTransferObject);
+
+        return Ok(databaseApplicationUserAdminDataTransferObject);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<ApplicationUserAdminDataTransferObject>?>> GetAll()
+    {
+        var userName = User.Identity?.Name;
+
+        if (userName == null)
+        {
+            return Ok(null);
+        }
+
+        var applicationUserAdminDataTransferObjects = await _adminApplicationUserService.GetAllAsync();
+
+        return Ok(applicationUserAdminDataTransferObjects);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ApplicationUserAdminDataTransferObject?>> GetById(Guid id)
+    {
+        var userName = User.Identity?.Name;
+
+        if (userName == null)
+        {
+            return Ok(null);
+        }
+
+        var applicationUserAdminDataTransferObject = await _adminApplicationUserService.GetByIdAsync(id);
+
+        return Ok(applicationUserAdminDataTransferObject);
+    }
+}
