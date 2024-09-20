544b29bad01dd31b892d1697d2ed71d3a2f40146
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-19 22:15:12 -0600

    gawk -i inplace "{ gsub(/EntityLowercaseNamePlaceholder/, \"toDoList\") }; { print }" Intrepion.ToDo.BusinessLogic/Services/IToDoListAdminService.cs

--- Intrepion.ToDo.BusinessLogic/Services/IToDoListAdminService.cs
+++ Intrepion.ToDo.BusinessLogic/Services/IToDoListAdminService.cs
@@ -4,9 +4,9 @@ namespace ApplicationNamePlaceholder.BusinessLogic.Services;
 
 public interface IEntityNamePlaceholderAdminService
 {
-    Task<EntityNamePlaceholderAdminDataTransferObject?> AddAsync(string userName, EntityNamePlaceholderAdminDataTransferObject EntityLowercaseNamePlaceholder);
+    Task<EntityNamePlaceholderAdminDataTransferObject?> AddAsync(string userName, EntityNamePlaceholderAdminDataTransferObject toDoList);
     Task<bool> DeleteAsync(string userName, Guid id);
-    Task<EntityNamePlaceholderAdminDataTransferObject?> EditAsync(string userName, Guid id, EntityNamePlaceholderAdminDataTransferObject EntityLowercaseNamePlaceholder);
+    Task<EntityNamePlaceholderAdminDataTransferObject?> EditAsync(string userName, Guid id, EntityNamePlaceholderAdminDataTransferObject toDoList);
     Task<List<EntityNamePlaceholderAdminDataTransferObject>?> GetAllAsync();
     Task<EntityNamePlaceholderAdminDataTransferObject?> GetByIdAsync(Guid id);
 }
