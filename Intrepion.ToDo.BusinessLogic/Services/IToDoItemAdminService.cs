f8f5579bd292e3f7f3090c0d6805a333c559043c
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-19 22:14:44 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Services/IEntityNamePlaceholderAdminService.cs Intrepion.ToDo.BusinessLogic/Services/IToDoItemAdminService.cs

---
+++ Intrepion.ToDo.BusinessLogic/Services/IToDoItemAdminService.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities.DataTransferObjects;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Services;
+
+public interface IEntityNamePlaceholderAdminService
+{
+    Task<EntityNamePlaceholderAdminDataTransferObject?> AddAsync(string userName, EntityNamePlaceholderAdminDataTransferObject EntityLowercaseNamePlaceholder);
+    Task<bool> DeleteAsync(string userName, Guid id);
+    Task<EntityNamePlaceholderAdminDataTransferObject?> EditAsync(string userName, Guid id, EntityNamePlaceholderAdminDataTransferObject EntityLowercaseNamePlaceholder);
+    Task<List<EntityNamePlaceholderAdminDataTransferObject>?> GetAllAsync();
+    Task<EntityNamePlaceholderAdminDataTransferObject?> GetByIdAsync(Guid id);
+}
