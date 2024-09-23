1016e208961ed9451cebc8807f8ed06161418d1d
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 01:57:08 -0600

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public List<ToDoItem>? ToDoItems { get; set; } = [];\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/ToDoList.cs

--- Intrepion.ToDo.BusinessLogic/Entities/ToDoList.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/ToDoList.cs
@@ -5,5 +5,6 @@ public class ToDoList
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
 
+    public List<ToDoItem>? ToDoItems { get; set; } = [];
     // ActualPropertyPlaceholder
 }
