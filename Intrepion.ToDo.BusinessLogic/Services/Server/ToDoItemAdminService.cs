c25ed76007187974dd3aaf1e4029835dc261b499
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 01:56:59 -0600

    gawk -i inplace "{ gsub(/\/\/ EditDatabasePropertyCodePlaceholder/, \"databaseToDoItem.NormalizedTitle = toDoItemAdminDto.Title.ToUpperInvariant();\n\ \ \ \ \ \ \ \ // EditDatabasePropertyCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Services/Server/ToDoItemAdminService.cs

--- Intrepion.ToDo.BusinessLogic/Services/Server/ToDoItemAdminService.cs
+++ Intrepion.ToDo.BusinessLogic/Services/Server/ToDoItemAdminService.cs
@@ -99,6 +99,7 @@ public class ToDoItemAdminService(ApplicationDbContext applicationDbContext) : I
         databaseToDoItem.ApplicationUserUpdatedBy = user;
 
         databaseToDoItem.Title = toDoItemAdminDto.Title;
+        databaseToDoItem.NormalizedTitle = toDoItemAdminDto.Title.ToUpperInvariant();
         // EditDatabasePropertyCodePlaceholder
         // databaseToDoItem.Title = toDoItemAdminDto.Title;
         // databaseToDoItem.NormalizedTitle = toDoItemAdminDto.Title.ToUpperInvariant();
