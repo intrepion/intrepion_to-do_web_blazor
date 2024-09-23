83fc37bd0a47faf4980bb00c9a95d102c0295da4
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 01:55:50 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder/* Intrepion.ToDo/

---
+++ Intrepion.ToDo/Controllers/ApplicationUserAdminController.cs
@@ -0,0 +1,112 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
+using ApplicationNamePlaceholder.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationNamePlaceholder.Controllers;
+
+[Route("api/admin/[controller]")]
+[ApiController]
+public class ApplicationUserController(IApplicationUserAdminService adminApplicationUserService) : ControllerBase
+{
+    private readonly IApplicationUserAdminService _adminApplicationUserService = adminApplicationUserService;
+
+    [HttpPost]
+    public async Task<ActionResult<ApplicationUserAdminDto?>> Add(ApplicationUserAdminDto applicationUserAdminDto)
+    {
+        var userIdentityName = User.Identity?.Name;
+
+        if (string.IsNullOrWhiteSpace(userIdentityName))
+        {
+            return Ok(null);
+        }
+
+        if (string.Equals(applicationUserAdminDto.ApplicationUserName, userIdentityName, StringComparison.InvariantCultureIgnoreCase))
+        {
+            return Ok(null);
+        }
+
+        var databaseApplicationUserAdminDto = await _adminApplicationUserService.AddAsync(applicationUserAdminDto);
+
+        return Ok(databaseApplicationUserAdminDto);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<bool?>> Delete(string userName, Guid id)
+    {
+        var userIdentityName = User.Identity?.Name;
+
+        if (string.IsNullOrWhiteSpace(userIdentityName))
+        {
+            return Ok(null);
+        }
+
+        if (string.Equals(userName, userIdentityName, StringComparison.InvariantCultureIgnoreCase))
+        {
+            return Ok(null);
+        }
+
+        var result = await _adminApplicationUserService.DeleteAsync(userIdentityName, id);
+
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<ApplicationUserAdminDto?>> Edit(ApplicationUserAdminDto applicationUserAdminDto)
+    {
+        var userIdentityName = User.Identity?.Name;
+
+        if (string.IsNullOrWhiteSpace(userIdentityName))
+        {
+            return Ok(null);
+        }
+
+        if (string.Equals(applicationUserAdminDto.ApplicationUserName, userIdentityName, StringComparison.InvariantCultureIgnoreCase))
+        {
+            return Ok(null);
+        }
+
+        var databaseApplicationUserAdminDto = await _adminApplicationUserService.EditAsync(applicationUserAdminDto);
+
+        return Ok(databaseApplicationUserAdminDto);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<ApplicationUserAdminDto>?>> GetAll(string userName)
+    {
+        var userIdentityName = User.Identity?.Name;
+
+        if (string.IsNullOrWhiteSpace(userIdentityName))
+        {
+            return Ok(null);
+        }
+
+        if (string.Equals(userName, userIdentityName, StringComparison.InvariantCultureIgnoreCase))
+        {
+            return Ok(null);
+        }
+
+        var applicationUserAdminDtos = await _adminApplicationUserService.GetAllAsync(userIdentityName);
+
+        return Ok(applicationUserAdminDtos);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ApplicationUserAdminDto?>> GetById(string userName, Guid id)
+    {
+        var userIdentityName = User.Identity?.Name;
+
+        if (string.IsNullOrWhiteSpace(userIdentityName))
+        {
+            return Ok(null);
+        }
+
+        if (string.Equals(userName, userIdentityName, StringComparison.InvariantCultureIgnoreCase))
+        {
+            return Ok(null);
+        }
+
+        var applicationUserAdminDto = await _adminApplicationUserService.GetByIdAsync(userIdentityName, id);
+
+        return Ok(applicationUserAdminDto);
+    }
+}
