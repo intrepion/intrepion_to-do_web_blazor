1e40aa3db3daf537a5e0ef2e72666fe2be6e58de
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 01:57:00 -0600

    gawk -i inplace "{ gsub(/\/\/ RequiredPropertyCodePlaceholder/, \"if (string.IsNullOrWhiteSpace(toDoItemAdminDto.Title))\n\ \ \ \ \ \ \ \ {\n\ \ \ \ \ \ \ \ \ \ \ \ throw new Exception(\\\"Title required.\\\");\n\ \ \ \ \ \ \ \ }\n\n\ \ \ \ \ \ \ \ // RequiredPropertyCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Services/Server/ToDoItemAdminService.cs

--- Intrepion.ToDo.BusinessLogic/Services/Server/ToDoItemAdminService.cs
+++ Intrepion.ToDo.BusinessLogic/Services/Server/ToDoItemAdminService.cs
@@ -23,6 +23,11 @@ public class ToDoItemAdminService(ApplicationDbContext applicationDbContext) : I
             throw new Exception("Authentication required.");
         }
 
+        if (string.IsNullOrWhiteSpace(toDoItemAdminDto.Title))
+        {
+            throw new Exception("Title required.");
+        }
+
         // RequiredPropertyCodePlaceholder
         // if (string.IsNullOrWhiteSpace(toDoItemAdminDto.Title))
         // {
