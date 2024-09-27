b4d8c5fcad851e49a58a4eedfa68a075ff3533ec
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-27 16:59:32 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder/* Intrepion.ToDo/

--- Intrepion.ToDo/Components/Account/IdentityRedirectManager.cs
+++ Intrepion.ToDo/Components/Account/IdentityRedirectManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Components;
 
-namespace Intrepion.ToDo.Components.Account;
+namespace ApplicationNamePlaceholder.Components.Account;
 
 internal sealed class IdentityRedirectManager(NavigationManager navigationManager)
 {
