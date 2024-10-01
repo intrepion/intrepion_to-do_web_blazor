4e07d4a3c56f9509d045404283e308ebe22ee371
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-01 13:35:10 -0600

    dotnet new nunit --name Intrepion.ToDo.ControllerTests

---
+++ Intrepion.ToDo.ControllerTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Intrepion.ToDo.ControllerTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
