9905b205b953ff6954d5e1ee56b2e24c9846c731
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-02 16:03:36 -0600

    gawk -i inplace "{ gsub(/\/\/ CreatePropertyCodePlaceholder/, \"await Page.GetByTestId(\\\"toDoItemAdminEditTitle\\\").FillAsync(\\\"a toDoItem\\\");\n\ \ \ \ \ \ \ \ // CreatePropertyCodePlaceholder\") }; { print }" Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs

--- Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
+++ Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
@@ -16,6 +16,7 @@ public partial class ToDoItemAdminPages : PageTest
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("To Do Item Creation");
 
+        await Page.GetByTestId("toDoItemAdminEditTitle").FillAsync("a toDoItem");
         // CreatePropertyCodePlaceholder
         // await Page.GetByTestId("toDoItemAdminEditName").FillAsync("a toDoItem");
 
