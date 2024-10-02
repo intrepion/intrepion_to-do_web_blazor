7f6b237dd0df2a2ce2d10b3d31cb7fd3749d8029
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-02 16:03:41 -0600

    gawk -i inplace "{ gsub(/\/\/ DtoPropertyPlaceholder/, \"public string Title { get; set; } = string.Empty;\n\ \ \ \ // DtoPropertyPlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Dtos/ToDoItemAdminDto.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Dtos/ToDoItemAdminDto.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Dtos/ToDoItemAdminDto.cs
@@ -6,6 +6,7 @@ public class ToDoItemAdminDto
     public Guid Id { get; set; }
 
     public ToDoList? ToDoList { get; set; }
+    public string Title { get; set; } = string.Empty;
     // DtoPropertyPlaceholder
     // public string Title { get; set; } = string.Empty;
     // public ToDoList? ToDoList { get; set; }
