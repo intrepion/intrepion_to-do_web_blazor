d4e8966d11df6f91ae072a2595e2b6f3f7626d93
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-02 20:56:44 -0600

    gawk -i inplace "{ gsub(/ClientUriPlaceholder/, \"http://localhost:5205\") }; { print }" Intrepion.ToDo.AcceptanceTests/Admin/*

--- Intrepion.ToDo.AcceptanceTests/Admin/ApplicationUserAdminPages.cs
+++ Intrepion.ToDo.AcceptanceTests/Admin/ApplicationUserAdminPages.cs
@@ -31,7 +31,7 @@ public partial class ApplicationUserAdminPages : PageTest
     [SetUp]
     public async Task SetUp()
     {
-        await Page.GotoAsync("ClientUriPlaceholder");
+        await Page.GotoAsync("http://localhost:5205");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Log in");
         await Page.GetByTestId("loginEmail").FillAsync("Admin1@Intrepion.ToDo.com");
