8df8b58cd02d12081cbb3e856e0c3b80818d3792
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-02 20:55:14 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/Dtos/EntityNamePlaceholderAdminDto.cs Intrepion.ToDo.BusinessLogic/Entities/Dtos/ToDoItemAdminDto.cs

---
+++ Intrepion.ToDo.BusinessLogic/Entities/Dtos/ToDoItemAdminDto.cs
@@ -0,0 +1,41 @@
+﻿namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
+
+public class EntityNamePlaceholderAdminDto
+{
+    public string ApplicationUserName { get; set; } = string.Empty;
+    public Guid Id { get; set; }
+
+    // DtoPropertyPlaceholder
+    // public string Title { get; set; } = string.Empty;
+    // public ToDoList? ToDoList { get; set; }
+
+    public static EntityNamePlaceholderAdminDto FromEntityNamePlaceholder(EntityNamePlaceholder? EntityLowercaseNamePlaceholder)
+    {
+        if (EntityLowercaseNamePlaceholder == null)
+        {
+            return new EntityNamePlaceholderAdminDto();
+        }
+
+        return new EntityNamePlaceholderAdminDto
+        {
+            Id = EntityLowercaseNamePlaceholder.Id,
+
+            // EntityToDtoPlaceholder
+            // Title = EntityLowercaseNamePlaceholder.Title,
+            // ToDoList = EntityLowercaseNamePlaceholder.ToDoList,
+        };
+    }
+
+    public static EntityNamePlaceholder ToEntityNamePlaceholder(ApplicationUser applicationUser, EntityNamePlaceholderAdminDto EntityLowercaseNamePlaceholderAdminDto)
+    {
+        return new EntityNamePlaceholder
+        {
+            ApplicationUserUpdatedBy = applicationUser,
+            Id = EntityLowercaseNamePlaceholderAdminDto.Id,
+
+            // DtoToEntityPropertyPlaceholder
+            // Title = EntityLowercaseNamePlaceholderAdminDto.Title,
+            // ToDoList = EntityLowercaseNamePlaceholderAdminDto.ToDoList,
+        };
+    }
+}
