f3d03dc2290228bbc30fbebe22f1d44a6c6c1aa1
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-02 20:55:04 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder.BusinessLogic/* Intrepion.ToDo.BusinessLogic/

---
+++ Intrepion.ToDo.BusinessLogic/Services/IApplicationRoleAdminService.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities;
+using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Services;
+
+public interface IApplicationRoleAdminService
+{
+    Task<ApplicationRoleAdminDto?> AddAsync(ApplicationRoleAdminDto applicationRoleAdminDto);
+    Task<bool> DeleteAsync(string userName, Guid id);
+    Task<ApplicationRoleAdminDto?> EditAsync(ApplicationRoleAdminDto applicationRoleAdminDto);
+    Task<List<ApplicationRole>?> GetAllAsync(string userName);
+    Task<ApplicationRoleAdminDto?> GetByIdAsync(string userName, Guid id);
+}
