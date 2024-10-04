b88c9366f582d5d2668807ca626ebaca46ad68a3
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-04 11:33:08 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder.BusinessLogic/* Intrepion.ToDo.BusinessLogic/

---
+++ Intrepion.ToDo.BusinessLogic/Services/IApplicationUserAdminService.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities;
+using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Services;
+
+public interface IApplicationUserAdminService
+{
+    Task<ApplicationUserAdminDto?> AddAsync(ApplicationUserAdminDto applicationUserAdminDto);
+    Task<bool> DeleteAsync(string userName, Guid id);
+    Task<ApplicationUserAdminDto?> EditAsync(ApplicationUserAdminDto applicationUserAdminDto);
+    Task<List<ApplicationUser>?> GetAllAsync(string userName);
+    Task<ApplicationUserAdminDto?> GetByIdAsync(string userName, Guid id);
+}
