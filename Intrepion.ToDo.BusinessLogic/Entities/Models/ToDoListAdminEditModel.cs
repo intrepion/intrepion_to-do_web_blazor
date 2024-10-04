a4a454bc0248f0978939e67b16ddef48aa7aed07
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-04 11:34:23 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/Models/EntityNamePlaceholderAdminEditModel.cs Intrepion.ToDo.BusinessLogic/Entities/Models/ToDoListAdminEditModel.cs

---
+++ Intrepion.ToDo.BusinessLogic/Entities/Models/ToDoListAdminEditModel.cs
@@ -0,0 +1,46 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Models;
+
+public class EntityNamePlaceholderAdminEditModel
+{
+    public Guid Id { get; set; }
+
+    // JustModelPropertyPlaceholder
+    // public string Title { get; set; } = string.Empty;
+    // public ToDoList? ToDoList { get; set; }
+
+    public static EntityNamePlaceholderAdminEditModel FromEntityNamePlaceholderAdminDto(EntityNamePlaceholderAdminDto EntityLowercaseNamePlaceholderAdminDto)
+    {
+        if (EntityLowercaseNamePlaceholderAdminDto == null)
+        {
+            return new EntityNamePlaceholderAdminEditModel();
+        }
+
+        return new EntityNamePlaceholderAdminEditModel
+        {
+            Id = EntityLowercaseNamePlaceholderAdminDto.Id,
+
+            // DtoToModelPlaceholder
+            // Title = EntityLowercaseNamePlaceholderAdminDto.Title,
+            // ToDoList = EntityLowercaseNamePlaceholderAdminDto.ToDoList,
+        };
+    }
+
+    public static EntityNamePlaceholderAdminDto ToEntityNamePlaceholderAdminDto(EntityNamePlaceholderAdminEditModel EntityLowercaseNamePlaceholderAdminEditModel)
+    {
+        if (EntityLowercaseNamePlaceholderAdminEditModel == null)
+        {
+            return new EntityNamePlaceholderAdminDto();
+        }
+
+        return new EntityNamePlaceholderAdminDto
+        {
+            Id = EntityLowercaseNamePlaceholderAdminEditModel.Id,
+
+            // ModelToDtoPlaceholder
+            // Title = EntityLowercaseNamePlaceholderAdminEditModel.Title,
+            // ToDoList = EntityLowercaseNamePlaceholderAdminEditModel.ToDoList,
+        };
+    }
+}
