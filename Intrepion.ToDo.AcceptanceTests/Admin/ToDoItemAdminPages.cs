480bd1f91293d97cdac9ea97eea4ce138597d50d
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-08 13:10:05 -0600

    gawk -i inplace "{ gsub(/PluralNamePlaceholder/, \"To Do Items\") }; { print }" Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs

--- Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
+++ Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
@@ -11,7 +11,7 @@ public partial class ToDoItemAdminPages : PageTest
     public async Task MainNavigation()
     {
         await Expect(Page).ToHaveTitleAsync("Home");
-        await Page.GetByRole(AriaRole.Link, new() { Name = "PluralNamePlaceholder" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "To Do Items" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("To Do Item List");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("To Do Item Creation");
