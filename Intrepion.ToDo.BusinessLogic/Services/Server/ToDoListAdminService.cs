59fc16d35bc5223532a02cacca3ce9d55d341f5c
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-15 00:07:30 -0600

    gawk -i inplace "{ gsub(/\/\/ EditDatabasePropertyCodePlaceholder/, \"databaseToDoList.Title = toDoListAdminDto.Title;\n\ \ \ \ \ \ \ \ // EditDatabasePropertyCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Services/Server/ToDoListAdminService.cs

--- Intrepion.ToDo.BusinessLogic/Services/Server/ToDoListAdminService.cs
+++ Intrepion.ToDo.BusinessLogic/Services/Server/ToDoListAdminService.cs
@@ -100,6 +100,7 @@ public class ToDoListAdminService(ApplicationDbContext applicationDbContext) : I
 
         databaseToDoList.ApplicationUserUpdatedBy = user;
 
+        databaseToDoList.Title = toDoListAdminDto.Title;
         // EditDatabasePropertyCodePlaceholder
         // databaseToDoList.Title = toDoListAdminDto.Title;
         // databaseToDoList.NormalizedTitle = toDoListAdminDto.Title.ToUpperInvariant();
