36d7bea6e62df65010ef8878b73d0c35a8cd0206
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-15 00:06:17 -0600

    gawk -i inplace "{ gsub(/\/\/ JustModelPropertyPlaceholder/, \"public string Title { get; set; } = string.Empty;\n\ \ \ \ // JustModelPropertyPlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Models/ToDoItemAdminEditModel.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Models/ToDoItemAdminEditModel.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Models/ToDoItemAdminEditModel.cs
@@ -9,6 +9,7 @@ public class ToDoItemAdminEditModel
     public bool IsCompleted { get; set; }
     public int Ordering { get; set; }
     public ToDoList? ToDoList { get; set; }
+    public string Title { get; set; } = string.Empty;
     // JustModelPropertyPlaceholder
     // public string Title { get; set; } = string.Empty;
     // public ToDoList? ToDoList { get; set; }
