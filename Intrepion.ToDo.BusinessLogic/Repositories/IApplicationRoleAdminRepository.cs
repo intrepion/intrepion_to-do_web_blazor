3e2a678d88f5f73e1bedfadf57472b9d2137ec22
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-17 00:02:36 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder.BusinessLogic/* Intrepion.ToDo.BusinessLogic/

---
+++ Intrepion.ToDo.BusinessLogic/Repositories/IApplicationRoleAdminRepository.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Repositories;
+
+public interface IApplicationRoleAdminRepository
+{
+    Task<ApplicationRoleAdminDto?> AddAsync(ApplicationRoleAdminDto applicationRoleAdminDto);
+    Task<bool> DeleteAsync(string userName, Guid id);
+    Task<ApplicationRoleAdminDto?> EditAsync(ApplicationRoleAdminDto applicationRoleAdminDto);
+    Task<List<ApplicationRoleAdminDto>?> GetAllAsync(string userName);
+    Task<ApplicationRoleAdminDto?> GetByIdAsync(string userName, Guid id);
+}
---
+++ Intrepion.ToDo.BusinessLogic/Repositories/IApplicationUserAdminRepository.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Repositories;
+
+public interface IApplicationUserAdminRepository
+{
+    Task<ApplicationUserAdminDto?> AddAsync(ApplicationUserAdminDto applicationUserAdminDto);
+    Task<bool> DeleteAsync(string userName, Guid id);
+    Task<ApplicationUserAdminDto?> EditAsync(ApplicationUserAdminDto applicationUserAdminDto);
+    Task<List<ApplicationUserAdminDto>?> GetAllAsync(string userName);
+    Task<ApplicationUserAdminDto?> GetByIdAsync(string userName, Guid id);
+}
