040e6caeb3d74c66002034473477b5d2b19b2c4f
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-17 00:04:41 -0600

    gawk -i inplace "{ gsub(/\/\/ DtoToModelPlaceholder/, \"Title = toDoItemAdminDto.Title,\n\ \ \ \ \ \ \ \ \ \ \ \ // DtoToModelPlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Models/ToDoItemAdminEditModel.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Models/ToDoItemAdminEditModel.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Models/ToDoItemAdminEditModel.cs
@@ -26,6 +26,7 @@ public class ToDoItemAdminEditModel
             IsCompleted = toDoItemAdminDto.IsCompleted,
             Ordering = toDoItemAdminDto.Ordering,
             ToDoList = toDoItemAdminDto.ToDoList,
+            Title = toDoItemAdminDto.Title,
             // DtoToModelPlaceholder
         };
     }
