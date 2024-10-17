31a664580d40cf49cc21b8de04e2a0597d151098
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-17 00:04:32 -0600

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public string NormalizedTitle { get; set; } = string.Empty;\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs

--- Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs
@@ -9,5 +9,6 @@ public class ToDoItem
     public int Ordering { get; set; }
     public ToDoList? ToDoList { get; set; }
     public string Title { get; set; } = string.Empty;
+    public string NormalizedTitle { get; set; } = string.Empty;
     // ActualPropertyPlaceholder
 }
