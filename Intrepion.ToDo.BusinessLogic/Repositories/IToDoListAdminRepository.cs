1c59a1f7cd08824824d1faf7052939584d056504
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-17 00:05:22 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"ToDoList\") }; { print }" Intrepion.ToDo.BusinessLogic/Repositories/IToDoListAdminRepository.cs

--- Intrepion.ToDo.BusinessLogic/Repositories/IToDoListAdminRepository.cs
+++ Intrepion.ToDo.BusinessLogic/Repositories/IToDoListAdminRepository.cs
@@ -3,11 +3,11 @@ using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Repositories;
 
-public interface IEntityNamePlaceholderAdminRepository
+public interface IToDoListAdminRepository
 {
-    Task<EntityNamePlaceholderAdminDto?> AddAsync(EntityNamePlaceholderAdminDto toDoList);
+    Task<ToDoListAdminDto?> AddAsync(ToDoListAdminDto toDoList);
     Task<bool> DeleteAsync(string userName, Guid id);
-    Task<EntityNamePlaceholderAdminDto?> EditAsync(EntityNamePlaceholderAdminDto toDoList);
-    Task<List<EntityNamePlaceholderAdminDto>?> GetAllAsync(string userName);
-    Task<EntityNamePlaceholderAdminDto?> GetByIdAsync(string userName, Guid id);
+    Task<ToDoListAdminDto?> EditAsync(ToDoListAdminDto toDoList);
+    Task<List<ToDoListAdminDto>?> GetAllAsync(string userName);
+    Task<ToDoListAdminDto?> GetByIdAsync(string userName, Guid id);
 }
