444751e0698cc60965e4514cb44550bf5f8dd2a0
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-17 22:09:52 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder.BusinessLogic/* Intrepion.ToDo.BusinessLogic/

---
+++ Intrepion.ToDo.BusinessLogic/Entities/ApplicationUserLogin.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
+
+public class ApplicationUserLogin : IdentityUserLogin<Guid>
+{
+    public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
+}
