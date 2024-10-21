e5cecdab291144b18a37144fb52d46defb4956c2
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-21 03:40:41 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder.BusinessLogic/* Intrepion.ToDo.BusinessLogic/

---
+++ Intrepion.ToDo.BusinessLogic/Entities/ViewModels/Admin/ApplicationUserAdminEditViewModel.cs
@@ -0,0 +1,50 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos.Admin;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities.ViewModels.Admin;
+
+public class ApplicationUserAdminEditViewModel
+{
+    public List<ApplicationRole> ApplicationRoles { get; set; } = [];
+    public Guid Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+
+    public static ApplicationUserAdminEditViewModel FromApplicationUserAdminDto(ApplicationUserAdminDto applicationUserAdminDto)
+    {
+        if (applicationUserAdminDto == null)
+        {
+            return new ApplicationUserAdminEditViewModel();
+        }
+
+        var applicationUserAdminEditViewModel = new ApplicationUserAdminEditViewModel
+        {
+            ApplicationRoles = applicationUserAdminDto.ApplicationRoles,
+            Email = applicationUserAdminDto.Email,
+            Id = applicationUserAdminDto.Id,
+            PhoneNumber = applicationUserAdminDto.PhoneNumber,
+            UserName = applicationUserAdminDto.UserName,
+        };
+
+        return applicationUserAdminEditViewModel;
+    }
+
+    public static ApplicationUserAdminDto ToApplicationUserAdminDto(ApplicationUserAdminEditViewModel? applicationUserAdminEditViewModel)
+    {
+        if (applicationUserAdminEditViewModel == null)
+        {
+            return new ApplicationUserAdminDto();
+        }
+
+        var applicationUserAdminDto = new ApplicationUserAdminDto
+        {
+            ApplicationRoles = applicationUserAdminEditViewModel.ApplicationRoles,
+            Email = applicationUserAdminEditViewModel.Email,
+            Id = applicationUserAdminEditViewModel.Id,
+            PhoneNumber = applicationUserAdminEditViewModel.PhoneNumber,
+            UserName = applicationUserAdminEditViewModel.UserName,
+        };
+
+        return applicationUserAdminDto;
+    }
+}
---
+++ Intrepion.ToDo.BusinessLogic/Repositories/Admin/Client/ApplicationRoleClientAdminRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Json;
+using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos.Admin;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Repositories.Admin.Client;
+
+public class ApplicationRoleClientAdminRepository(HttpClient httpClient) : IApplicationRoleAdminRepository
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+    public async Task<ApplicationRoleAdminDto?> AddAsync(ApplicationRoleAdminDto applicationRoleAdminDto)
+    {
+        var result = await _httpClient.PostAsJsonAsync("/api/admin/applicationRoleAdmin", applicationRoleAdminDto);
+
+        return await result.Content.ReadFromJsonAsync<ApplicationRoleAdminDto>();
+    }
+
+    public async Task<bool> DeleteAsync(string userName, Guid id)
+    {
+        var result = await _httpClient.DeleteAsync($"/api/admin/applicationRoleAdmin/{id}?userName={userName}");
+
+        return await result.Content.ReadFromJsonAsync<bool>();
+    }
+
+    public async Task<ApplicationRoleAdminDto?> EditAsync(ApplicationRoleAdminDto applicationRoleAdminDto)
+    {
+        var result = await _httpClient.PutAsJsonAsync($"/api/admin/applicationRoleAdmin/{applicationRoleAdminDto.Id}", applicationRoleAdminDto);
+
+        return await result.Content.ReadFromJsonAsync<ApplicationRoleAdminDto>();
+    }
+
+    public async Task<List<ApplicationRoleAdminDto>?> GetAllAsync(string userName)
+    {
+        var result = await _httpClient.GetFromJsonAsync<List<ApplicationRoleAdminDto>>($"/api/admin/applicationRoleAdmin?userName={userName}");
+
+        return result;
+    }
+
+    public async Task<ApplicationRoleAdminDto?> GetByIdAsync(string userName, Guid id)
+    {
+        var result = await _httpClient.GetFromJsonAsync<ApplicationRoleAdminDto>($"/api/admin/applicationRoleAdmin/{id}?userName={userName}");
+
+        return result;
+    }
+}
