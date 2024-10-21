e94827306d97cc1b55cfb064f8dc93e5cbe39a39
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-21 03:42:34 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Repositories/Admin/IEntityNamePlaceholderAdminRepository.cs Intrepion.ToDo.BusinessLogic/Repositories/Admin/IToDoListAdminRepository.cs

---
+++ Intrepion.ToDo.BusinessLogic/Repositories/Admin/IToDoListAdminRepository.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos.Admin;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Repositories.Admin;
+
+public interface IEntityNamePlaceholderAdminRepository
+{
+    Task<EntityNamePlaceholderAdminDto?> AddAsync(EntityNamePlaceholderAdminDto EntityLowercaseNamePlaceholder);
+    Task<bool> DeleteAsync(string userName, Guid id);
+    Task<EntityNamePlaceholderAdminDto?> EditAsync(EntityNamePlaceholderAdminDto EntityLowercaseNamePlaceholder);
+    Task<List<EntityNamePlaceholderAdminDto>?> GetAllAsync(string userName);
+    Task<EntityNamePlaceholderAdminDto?> GetByIdAsync(string userName, Guid id);
+}
