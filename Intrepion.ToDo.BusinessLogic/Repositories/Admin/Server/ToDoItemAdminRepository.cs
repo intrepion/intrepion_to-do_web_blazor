80bafa16fd42985e73b71e8492db155eb79d81d9
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-27 16:39:26 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Repositories/Admin/Server/EntityNamePlaceholderAdminRepository.cs Intrepion.ToDo.BusinessLogic/Repositories/Admin/Server/ToDoItemAdminRepository.cs

---
+++ Intrepion.ToDo.BusinessLogic/Repositories/Admin/Server/ToDoItemAdminRepository.cs
@@ -0,0 +1,146 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Data;
+using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos.Admin;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Repositories.Admin.Server;
+
+public class EntityNamePlaceholderAdminRepository(ApplicationDbContext applicationDbContext) : IEntityNamePlaceholderAdminRepository
+{
+    private readonly ApplicationDbContext _applicationDbContext = applicationDbContext;
+
+    public async Task<EntityNamePlaceholderAdminDto?> AddAsync(EntityNamePlaceholderAdminDto EntityLowercaseNamePlaceholderAdminDto)
+    {
+        if (string.IsNullOrWhiteSpace(EntityLowercaseNamePlaceholderAdminDto.ApplicationUserName))
+        {
+            throw new Exception("UserName is required.");
+        }
+
+        var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => EntityLowercaseNamePlaceholderAdminDto.ApplicationUserName.ToUpper().Equals(x.NormalizedUserName));
+
+        if (user == null)
+        {
+            throw new Exception("Authentication required.");
+        }
+
+        // AddRequiredPropertyCodePlaceholder
+
+        var EntityLowercaseNamePlaceholder = EntityNamePlaceholderAdminDto.ToEntityNamePlaceholder(user, EntityLowercaseNamePlaceholderAdminDto);
+
+        // AddDatabasePropertyCodePlaceholder
+
+        var result = await _applicationDbContext.TableNamePlaceholder.AddAsync(EntityLowercaseNamePlaceholder);
+        var databaseEntityNamePlaceholderAdminDto = EntityNamePlaceholderAdminDto.FromEntityNamePlaceholder(result.Entity);
+        await _applicationDbContext.SaveChangesAsync();
+
+        return databaseEntityNamePlaceholderAdminDto;
+    }
+
+    public async Task<bool> DeleteAsync(string userName, Guid id)
+    {
+        if (string.IsNullOrWhiteSpace(userName))
+        {
+            throw new Exception("UserName is required.");
+        }
+
+        var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => userName.ToUpper().Equals(x.NormalizedUserName));
+
+        if (user == null)
+        {
+            throw new Exception("Authentication required.");
+        }
+
+        var databaseEntityNamePlaceholder = await _applicationDbContext.TableNamePlaceholder.FindAsync(id);
+
+        if (databaseEntityNamePlaceholder == null)
+        {
+            return false;
+        }
+
+        databaseEntityNamePlaceholder.ApplicationUserUpdatedBy = user;
+        await _applicationDbContext.SaveChangesAsync();
+
+        _applicationDbContext.Remove(databaseEntityNamePlaceholder);
+
+        await _applicationDbContext.SaveChangesAsync();
+
+        return true;
+    }
+
+    public async Task<EntityNamePlaceholderAdminDto?> EditAsync(EntityNamePlaceholderAdminDto EntityLowercaseNamePlaceholderAdminDto)
+    {
+        if (string.IsNullOrWhiteSpace(EntityLowercaseNamePlaceholderAdminDto.ApplicationUserName))
+        {
+            throw new Exception("UserName is required.");
+        }
+
+        var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => EntityLowercaseNamePlaceholderAdminDto.ApplicationUserName.ToUpper().Equals(x.NormalizedUserName));
+
+        if (user == null)
+        {
+            throw new Exception("Authentication required.");
+        }
+
+        var databaseEntityNamePlaceholder = await _applicationDbContext.TableNamePlaceholder.FindAsync(EntityLowercaseNamePlaceholderAdminDto.Id);
+
+        if (databaseEntityNamePlaceholder == null)
+        {
+            throw new Exception("HumanNamePlaceholder not found.");
+        }
+
+        // EditRequiredPropertyCodePlaceholder
+
+        databaseEntityNamePlaceholder.ApplicationUserUpdatedBy = user;
+
+        // EditDatabasePropertyCodePlaceholder
+
+        await _applicationDbContext.SaveChangesAsync();
+
+        return EntityLowercaseNamePlaceholderAdminDto;
+    }
+
+    public async Task<List<EntityNamePlaceholderAdminDto>?> GetAllAsync(string userName)
+    {
+        if (string.IsNullOrWhiteSpace(userName))
+        {
+            throw new Exception("UserName is required.");
+        }
+
+        var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => userName.ToUpper().Equals(x.NormalizedUserName));
+
+        if (user == null)
+        {
+            throw new Exception("Authentication required.");
+        }
+
+        return await _applicationDbContext.TableNamePlaceholder
+
+            // IncludeTableCodePlaceholder
+
+            .Select(x => EntityNamePlaceholderAdminDto.FromEntityNamePlaceholder(x))
+            .ToListAsync();
+    }
+
+    public async Task<EntityNamePlaceholderAdminDto?> GetByIdAsync(string userName, Guid id)
+    {
+        if (string.IsNullOrWhiteSpace(userName))
+        {
+            throw new Exception("UserName is required.");
+        }
+
+        var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => userName.ToUpper().Equals(x.NormalizedUserName));
+
+        if (user == null)
+        {
+            throw new Exception("Authentication required.");
+        }
+
+        var result = await _applicationDbContext.TableNamePlaceholder.FindAsync(id);
+
+        if (result == null)
+        {
+            return null;
+        }
+
+        return EntityNamePlaceholderAdminDto.FromEntityNamePlaceholder(result);
+    }
+}
