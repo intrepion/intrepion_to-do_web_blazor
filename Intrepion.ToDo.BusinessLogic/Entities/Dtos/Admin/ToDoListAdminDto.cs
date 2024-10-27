3e40fde951ea26b9a6ced5dba3222b0339a266bf
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-27 16:41:19 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/Dtos/Admin/EntityNamePlaceholderAdminDto.cs Intrepion.ToDo.BusinessLogic/Entities/Dtos/Admin/ToDoListAdminDto.cs

---
+++ Intrepion.ToDo.BusinessLogic/Entities/Dtos/Admin/ToDoListAdminDto.cs
@@ -0,0 +1,35 @@
+﻿namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos.Admin;
+
+public class EntityNamePlaceholderAdminDto
+{
+    public string ApplicationUserName { get; set; } = string.Empty;
+    public Guid Id { get; set; }
+
+    // DtoPropertyPlaceholder
+
+    public static EntityNamePlaceholderAdminDto FromEntityNamePlaceholder(EntityNamePlaceholder? EntityLowercaseNamePlaceholder)
+    {
+        if (EntityLowercaseNamePlaceholder == null)
+        {
+            return new EntityNamePlaceholderAdminDto();
+        }
+
+        return new EntityNamePlaceholderAdminDto
+        {
+            Id = EntityLowercaseNamePlaceholder.Id,
+
+            // EntityToDtoPlaceholder
+        };
+    }
+
+    public static EntityNamePlaceholder ToEntityNamePlaceholder(ApplicationUser? applicationUser, EntityNamePlaceholderAdminDto? EntityLowercaseNamePlaceholderAdminDto)
+    {
+        return new EntityNamePlaceholder
+        {
+            ApplicationUserUpdatedBy = applicationUser ?? new ApplicationUser(),
+            Id = EntityLowercaseNamePlaceholderAdminDto?.Id ?? new Guid(),
+
+            // DtoToEntityPropertyPlaceholder
+        };
+    }
+}
