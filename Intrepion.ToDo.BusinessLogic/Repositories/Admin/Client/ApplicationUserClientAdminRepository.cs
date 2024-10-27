3924ce2d0a7e98fb13117924f43f08194d9ec133
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-27 16:38:44 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder.BusinessLogic/* Intrepion.ToDo.BusinessLogic/

---
+++ Intrepion.ToDo.BusinessLogic/Repositories/Admin/Client/ApplicationUserClientAdminRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Json;
+using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos.Admin;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Repositories.Admin.Client;
+
+public class ApplicationUserClientAdminRepository(HttpClient httpClient) : IApplicationUserAdminRepository
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+    public async Task<ApplicationUserAdminDto?> AddAsync(ApplicationUserAdminDto applicationUserAdminDto)
+    {
+        var result = await _httpClient.PostAsJsonAsync("/api/admin/applicationUserAdmin", applicationUserAdminDto);
+
+        return await result.Content.ReadFromJsonAsync<ApplicationUserAdminDto>();
+    }
+
+    public async Task<bool> DeleteAsync(string userName, Guid id)
+    {
+        var result = await _httpClient.DeleteAsync($"/api/admin/applicationUserAdmin/{id}?userName={userName}");
+
+        return await result.Content.ReadFromJsonAsync<bool>();
+    }
+
+    public async Task<ApplicationUserAdminDto?> EditAsync(ApplicationUserAdminDto applicationUserAdminDto)
+    {
+        var result = await _httpClient.PutAsJsonAsync($"/api/admin/applicationUserAdmin/{applicationUserAdminDto.Id}", applicationUserAdminDto);
+
+        return await result.Content.ReadFromJsonAsync<ApplicationUserAdminDto>();
+    }
+
+    public async Task<List<ApplicationUserAdminDto>?> GetAllAsync(string userName)
+    {
+        var result = await _httpClient.GetFromJsonAsync<List<ApplicationUserAdminDto>>($"/api/admin/applicationUserAdmin?userName={userName}");
+
+        return result;
+    }
+
+    public async Task<ApplicationUserAdminDto?> GetByIdAsync(string userName, Guid id)
+    {
+        var result = await _httpClient.GetFromJsonAsync<ApplicationUserAdminDto>($"/api/admin/applicationUserAdmin/{id}?userName={userName}");
+
+        return result;
+    }
+}
---
+++ Intrepion.ToDo.BusinessLogic/Repositories/Admin/IApplicationRoleAdminRepository.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos.Admin;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Repositories.Admin;
+
+public interface IApplicationRoleAdminRepository
+{
+    Task<ApplicationRoleAdminDto?> AddAsync(ApplicationRoleAdminDto applicationRoleAdminDto);
+    Task<bool> DeleteAsync(string userName, Guid id);
+    Task<ApplicationRoleAdminDto?> EditAsync(ApplicationRoleAdminDto applicationRoleAdminDto);
+    Task<List<ApplicationRoleAdminDto>?> GetAllAsync(string userName);
+    Task<ApplicationRoleAdminDto?> GetByIdAsync(string userName, Guid id);
+}
