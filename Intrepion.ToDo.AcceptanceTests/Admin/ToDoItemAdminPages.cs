044e7b324cfe0b616d5948b774a84965925b316b
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-27 16:43:11 -0600

    dotnet format

--- Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
+++ Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
@@ -45,7 +45,7 @@ public partial class ToDoItemAdminPages : PageTest
         await Page.GotoAsync(baseUrl);
 
 
-       await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Log in");
         await Page.GetByTestId("loginEmail").FillAsync("Admin1@Intrepion.ToDo.com");
         await Page.GetByTestId("loginPassword").FillAsync("Admin1@Intrepion.ToDo.com");
