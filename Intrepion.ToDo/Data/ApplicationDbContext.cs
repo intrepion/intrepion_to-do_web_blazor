0f54428ab967a58119197d3c9498600cd57b9999
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-29 08:35:50 -0600

    rm -rf Intrepion.ToDo/Data

--- Intrepion.ToDo/Data/ApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Intrepion.ToDo.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-{
-}
