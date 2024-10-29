f80517fce18ddde721e748dc6aa41e57b85955b5
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-29 08:35:11 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/Vms/Admin/EntityNamePlaceholderAdminEditVm.cs Intrepion.ToDo.BusinessLogic/Entities/Vms/Admin/ToDoListAdminEditVm.cs

---
+++ Intrepion.ToDo.BusinessLogic/Entities/Vms/Admin/ToDoListAdminEditVm.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos.Admin;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Vms.Admin;
+
+public class EntityNamePlaceholderAdminEditVm
+{
+    public Guid Id { get; set; }
+
+    // JustModelPropertyPlaceholder
+
+    public static EntityNamePlaceholderAdminEditVm FromEntityNamePlaceholderAdminDto(EntityNamePlaceholderAdminDto EntityLowercaseNamePlaceholderAdminDto)
+    {
+        if (EntityLowercaseNamePlaceholderAdminDto == null)
+        {
+            return new EntityNamePlaceholderAdminEditVm();
+        }
+
+        return new EntityNamePlaceholderAdminEditVm
+        {
+            Id = EntityLowercaseNamePlaceholderAdminDto.Id,
+
+            // DtoToModelPlaceholder
+        };
+    }
+
+    public static EntityNamePlaceholderAdminDto ToEntityNamePlaceholderAdminDto(EntityNamePlaceholderAdminEditVm EntityLowercaseNamePlaceholderAdminEditVm)
+    {
+        if (EntityLowercaseNamePlaceholderAdminEditVm == null)
+        {
+            return new EntityNamePlaceholderAdminDto();
+        }
+
+        return new EntityNamePlaceholderAdminDto
+        {
+            Id = EntityLowercaseNamePlaceholderAdminEditVm.Id,
+
+            // ModelToDtoPlaceholder
+        };
+    }
+}
