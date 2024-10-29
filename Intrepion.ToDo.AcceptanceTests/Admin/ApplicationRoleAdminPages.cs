d97f4ada63d07a9ce19303ff7e91b19adaed642b
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-28 19:51:42 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder.AcceptanceTests/* Intrepion.ToDo.AcceptanceTests/

---
+++ Intrepion.ToDo.AcceptanceTests/Admin/ApplicationRoleAdminPages.cs
@@ -0,0 +1,52 @@
+﻿using Bogus;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace ApplicationNamePlaceholder.AcceptanceTests.Admin;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public partial class ApplicationRoleAdminPages : PageTest
+{
+    [Test]
+    public async Task MainNavigation()
+    {
+        var faker = new Faker();
+        string aRandomString = faker.Random.String2(10);
+        string someRandomString = faker.Random.String2(10);
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Application Roles" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Application Role List");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Create" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Application Role Creation");
+        await Page.GetByTestId("applicationRoleAdminEditName").FillAsync("aName" + aRandomString);
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Create" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Application Role Modification");
+        await Page.GetByTestId("applicationRoleAdminEditName").FillAsync("someName" + someRandomString);
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Modify" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Application Role Modification");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Remove" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Application Role List");
+    }
+
+    [SetUp]
+    public async Task SetUp()
+    {
+        var baseUrl = Environment.GetEnvironmentVariable("BASE_URL");
+        if (string.IsNullOrEmpty(baseUrl))
+        {
+            baseUrl = "ClientUriPlaceholder";
+        }
+        await Page.GotoAsync(baseUrl);
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Log in");
+        await Page.GetByTestId("loginEmail").FillAsync("Admin1@ApplicationNamePlaceholder.com");
+        await Page.GetByTestId("loginPassword").FillAsync("Admin1@ApplicationNamePlaceholder.com");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Logout" }).ClickAsync();
+    }
+}
