b3ff066eef5c023fc8cbd1a6672b0dd43e73b72a
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-03 01:47:27 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"ToDoList\") }; { print }" Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoListGrid/ToDoListGridControls.cs

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoListGrid/ToDoListGridControls.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoListGrid/ToDoListGridControls.cs
@@ -1,7 +1,7 @@
-namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.ToDoListGrid;
 
 // State of grid filters.
-public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntityNamePlaceholderFilters
+public class ToDoListGridControls(IPageHelper pageHelper) : IToDoListFilters
 {
     // Keep state of paging.
     public IPageHelper PageHelper { get; set; } = pageHelper;
@@ -10,13 +10,13 @@ public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntity
     public bool Loading { get; set; }
 
     // Column to sort by.
-    public EntityNamePlaceholderFilterColumns SortColumn { get; set; } = EntityNamePlaceholderFilterColumns.Title;
+    public ToDoListFilterColumns SortColumn { get; set; } = ToDoListFilterColumns.Title;
 
     // True when sorting ascending, otherwise sort descending.
     public bool SortAscending { get; set; } = true;
 
     // Column filtered text is against.
-    public EntityNamePlaceholderFilterColumns FilterColumn { get; set; } = EntityNamePlaceholderFilterColumns.Title;
+    public ToDoListFilterColumns FilterColumn { get; set; } = ToDoListFilterColumns.Title;
 
     // Text to filter on.
     public string? FilterText { get; set; }
