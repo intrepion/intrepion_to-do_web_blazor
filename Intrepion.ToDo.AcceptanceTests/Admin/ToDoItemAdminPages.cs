e487f348a046c87ab01324c9e426e2abdc5b432e
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-04 12:36:05 -0700

    gawk -i inplace "{ gsub(/\/\/ ModifyPropertyCodePlaceholder/, \"await Page.GetByLabel(\\\"Ordering:\\\").FillAsync(\\\"2\\\");\n\ \ \ \ \ \ \ \ // ModifyPropertyCodePlaceholder\") }; { print }" Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs

--- Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
+++ Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
@@ -29,6 +29,7 @@ public partial class ToDoItemAdminPages : PageTest
         await Expect(Page).ToHaveTitleAsync("To Do Item Edit");
 
         await Page.GetByLabel("IsCompleted:").CheckAsync();
+        await Page.GetByLabel("Ordering:").FillAsync("2");
         // ModifyPropertyCodePlaceholder
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
