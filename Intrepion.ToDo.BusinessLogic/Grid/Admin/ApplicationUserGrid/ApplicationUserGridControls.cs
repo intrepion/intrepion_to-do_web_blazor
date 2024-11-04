c5a6b3235a8e38bd6c58efb70553060c7b098683
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-04 12:37:58 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.ToDo\") }; { print }" Intrepion.ToDo.BusinessLogic/Grid/Admin/ApplicationUserGrid/*

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ApplicationUserGrid/ApplicationUserGridControls.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ApplicationUserGrid/ApplicationUserGridControls.cs
@@ -1,4 +1,4 @@
-namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationUserGrid;
+namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.ApplicationUserGrid;
 
 // State of grid filters.
 public class ApplicationUserGridControls(IPageHelper pageHelper) : IApplicationUserFilters
