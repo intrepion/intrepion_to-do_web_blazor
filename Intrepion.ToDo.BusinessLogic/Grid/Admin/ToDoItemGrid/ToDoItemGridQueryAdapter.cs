af85026dbd6adab81736acce99e2622054bdc069
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-04 12:36:26 -0700

    gawk -i inplace "{ gsub(/\/\/ SortExpressionCodePlaceholder/, \"{ ToDoItemFilterColumns.Title, c => c != null \\\\&\\\\& c.Title != null ? c.Title : string.Empty },\n\ \ \ \ \ \ \ \ \ \ \ \ // SortExpressionCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/ToDoItemGridQueryAdapter.cs

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/ToDoItemGridQueryAdapter.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/ToDoItemGridQueryAdapter.cs
@@ -19,6 +19,7 @@ public class ToDoItemGridQueryAdapter
 
             { ToDoItemFilterColumns.IsCompleted, c => c != null && c.IsCompleted != null ? c.IsCompleted.ToString() : string.Empty },
             { ToDoItemFilterColumns.Ordering, c => c != null && c.Ordering != null ? c.Ordering.ToString() : string.Empty },
+            { ToDoItemFilterColumns.Title, c => c != null && c.Title != null ? c.Title : string.Empty },
             // SortExpressionCodePlaceholder
             // { ToDoItemFilterColumns.Name, x => x != null && x.Name != null ? x.Name : string.Empty },
         };
