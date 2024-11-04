e9c58e0c8f8402cec5af6dde1b0ea8c23c5e851b
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-04 12:34:39 -0700

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder.BusinessLogic/* Intrepion.ToDo.BusinessLogic/

---
+++ Intrepion.ToDo.BusinessLogic/Entities/Etcs/ApplicationRoleEtc.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
+
+public class ApplicationRoleEtc : IEntityTypeConfiguration<ApplicationRole>
+{
+    public void Configure(EntityTypeBuilder<ApplicationRole> builder)
+    {
+        builder.ToTable("AspNetRoles", x => x.IsTemporal());
+
+        builder.HasOne(x => x.ApplicationUserUpdatedBy)
+            .WithMany(x => x.UpdatedApplicationRoles)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ApplicationUserGrid/IApplicationUserFilters.cs
@@ -0,0 +1,26 @@
+﻿namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationUserGrid;
+
+// Interface for filtering.
+public interface IApplicationUserFilters
+{
+    // The ApplicationUserFilterColumns being filtered on.
+    ApplicationUserFilterColumns FilterColumn { get; set; }
+
+    // Loading indicator.
+    bool Loading { get; set; }
+
+    // The text of the filter.
+    string? FilterText { get; set; }
+
+    // Paging state in PageHelper.
+    IPageHelper PageHelper { get; set; }
+
+    // Gets or sets a value indicating if the name is rendered first name first.
+    bool ShowUserNameFirst { get; set; }
+
+    // Gets or sets a value indicating if the sort is ascending or descending.
+    bool SortAscending { get; set; }
+
+    // The ApplicationUserFilterColumns being sorted.
+    ApplicationUserFilterColumns SortColumn { get; set; }
+}
