da601135dad03656805aff2afba8c3b139fae0e0
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-04 12:35:30 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"ToDoItem\") }; { print }" Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/ToDoItemGridControls.cs

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/ToDoItemGridControls.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/ToDoItemGridControls.cs
@@ -1,7 +1,7 @@
-namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.ToDoItemGrid;
 
 // State of grid filters.
-public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntityNamePlaceholderFilters
+public class ToDoItemGridControls(IPageHelper pageHelper) : IToDoItemFilters
 {
     // Keep state of paging.
     public IPageHelper PageHelper { get; set; } = pageHelper;
@@ -10,13 +10,13 @@ public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntity
     public bool Loading { get; set; }
 
     // Column to sort by.
-    public EntityNamePlaceholderFilterColumns SortColumn { get; set; } = EntityNamePlaceholderFilterColumns.Title;
+    public ToDoItemFilterColumns SortColumn { get; set; } = ToDoItemFilterColumns.Title;
 
     // True when sorting ascending, otherwise sort descending.
     public bool SortAscending { get; set; } = true;
 
     // Column filtered text is against.
-    public EntityNamePlaceholderFilterColumns FilterColumn { get; set; } = EntityNamePlaceholderFilterColumns.Title;
+    public ToDoItemFilterColumns FilterColumn { get; set; } = ToDoItemFilterColumns.Title;
 
     // Text to filter on.
     public string? FilterText { get; set; }
