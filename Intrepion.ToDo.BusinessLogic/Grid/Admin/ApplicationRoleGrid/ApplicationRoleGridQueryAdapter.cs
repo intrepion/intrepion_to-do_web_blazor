47c5b482179fa8a9b1a3ebfebd68e0b2e0c18408
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-04 12:37:57 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.ToDo\") }; { print }" Intrepion.ToDo.BusinessLogic/Grid/Admin/ApplicationRoleGrid/*

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ApplicationRoleGrid/ApplicationRoleGridQueryAdapter.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ApplicationRoleGrid/ApplicationRoleGridQueryAdapter.cs
@@ -1,9 +1,9 @@
 using System.Diagnostics;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
-using ApplicationNamePlaceholder.BusinessLogic.Entities;
+using Intrepion.ToDo.BusinessLogic.Entities;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationRoleGrid;
+namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.ApplicationRoleGrid;
 
 // Creates the correct expressions to filter and sort.
 public class ApplicationRoleGridQueryAdapter
