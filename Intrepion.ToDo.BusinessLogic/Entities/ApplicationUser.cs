0dc0f3ed4aeb803e064c63c1c4df7ef04396ea8e
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-05 10:44:46 -0700

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder.BusinessLogic/* Intrepion.ToDo.BusinessLogic/

---
+++ Intrepion.ToDo.BusinessLogic/Entities/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public ICollection<ApplicationUserRole> ApplicationUserRoles { get; set; } = [];
+    public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
+    public ICollection<ApplicationRole> UpdatedApplicationRoles { get; set; } = [];
+    public ICollection<ApplicationRoleClaim> UpdatedApplicationRoleClaims { get; set; } = [];
+    public ICollection<ApplicationUser> UpdatedApplicationUsers { get; set; } = [];
+    public ICollection<ApplicationUserClaim> UpdatedApplicationUserClaims { get; set; } = [];
+    public ICollection<ApplicationUserLogin> UpdatedApplicationUserLogins { get; set; } = [];
+    public ICollection<ApplicationUserRole> UpdatedApplicationUserRoles { get; set; } = [];
+    public ICollection<ApplicationUserToken> UpdatedApplicationUserTokens { get; set; } = [];
+
+    // ActualPropertyPlaceholder
+}
