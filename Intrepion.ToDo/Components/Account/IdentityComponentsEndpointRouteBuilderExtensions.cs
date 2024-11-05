2e37063a80808b2470f531901be8e083b2e3d5e7
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-05 10:38:47 -0700

    dotnet format

--- Intrepion.ToDo/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ Intrepion.ToDo/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -1,14 +1,14 @@
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Text.Json;
+using Intrepion.ToDo.Components.Account.Pages;
+using Intrepion.ToDo.Components.Account.Pages.Manage;
+using Intrepion.ToDo.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
-using Intrepion.ToDo.Components.Account.Pages;
-using Intrepion.ToDo.Components.Account.Pages.Manage;
-using Intrepion.ToDo.Data;
 
 namespace Microsoft.AspNetCore.Routing;
 
