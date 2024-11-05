15bcf3e8b4560909bd0a006ee9ce28fb7437900f
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-05 10:45:19 -0700

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Grid/Admin/EntityNamePlaceholderGrid/EntityNamePlaceholderGridControls.cs Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/ToDoItemGridControls.cs

---
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/ToDoItemGridControls.cs
@@ -0,0 +1,23 @@
+namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+
+// State of grid filters.
+public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntityNamePlaceholderFilters
+{
+    // Keep state of paging.
+    public IPageHelper PageHelper { get; set; } = pageHelper;
+
+    // Avoid multiple concurrent requests.
+    public bool Loading { get; set; }
+
+    // Column to sort by.
+    public EntityNamePlaceholderFilterColumns SortColumn { get; set; } = EntityNamePlaceholderFilterColumns.DefaultSortNamePlaceholder;
+
+    // True when sorting ascending, otherwise sort descending.
+    public bool SortAscending { get; set; } = DefaultAscendingNamePlaceholder;
+
+    // Column filtered text is against.
+    public EntityNamePlaceholderFilterColumns FilterColumn { get; set; } = EntityNamePlaceholderFilterColumns.DefaultFilterNamePlaceholder;
+
+    // Text to filter on.
+    public string? FilterText { get; set; }
+}
