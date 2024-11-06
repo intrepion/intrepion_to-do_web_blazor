57452a26d16b79b2f963814fc8d1fa8882906865
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-06 14:19:38 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.ToDo\") }; { print }" Intrepion.ToDo/Components/Account/*

--- Intrepion.ToDo/Components/Account/IdentityUserAccessor.cs
+++ Intrepion.ToDo/Components/Account/IdentityUserAccessor.cs
@@ -1,7 +1,7 @@
-﻿using ApplicationNamePlaceholder.BusinessLogic.Entities;
+﻿using Intrepion.ToDo.BusinessLogic.Entities;
 using Microsoft.AspNetCore.Identity;
 
-namespace ApplicationNamePlaceholder.Components.Account;
+namespace Intrepion.ToDo.Components.Account;
 
 internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
 {
