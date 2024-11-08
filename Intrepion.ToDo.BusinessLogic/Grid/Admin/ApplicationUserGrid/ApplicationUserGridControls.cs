dcdc9cdaf5f45ba25335a285d379715a2c10ab1c
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-08 13:50:51 -0700

    dotnet format

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ApplicationUserGrid/ApplicationUserGridControls.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ApplicationUserGrid/ApplicationUserGridControls.cs
@@ -1,4 +1,4 @@
-namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.ApplicationUserGrid;
+﻿namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.ApplicationUserGrid;
 
 // State of grid filters.
 public class ApplicationUserGridControls(IPageHelper pageHelper) : IApplicationUserFilters
