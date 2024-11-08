a2f540b91ece54984efe170bb9071975871629a2
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-08 13:49:49 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.ToDo\") }; { print }" Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoListGrid/ToDoListGridQueryAdapter.cs

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoListGrid/ToDoListGridQueryAdapter.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoListGrid/ToDoListGridQueryAdapter.cs
@@ -1,9 +1,9 @@
 using System.Diagnostics;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
-using ApplicationNamePlaceholder.BusinessLogic.Entities;
+using Intrepion.ToDo.BusinessLogic.Entities;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
 
 // Creates the correct expressions to filter and sort.
 public class EntityNamePlaceholderGridQueryAdapter
