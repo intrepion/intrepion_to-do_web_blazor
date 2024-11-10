4a321a65bdc67446fbf0501b13038ac62a184c92
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-09 20:23:56 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"toDoList\") }; { print }" Intrepion.ToDo.AcceptanceTests/Admin/ToDoListAdminPages.cs

--- Intrepion.ToDo.AcceptanceTests/Admin/ToDoListAdminPages.cs
+++ Intrepion.ToDo.AcceptanceTests/Admin/ToDoListAdminPages.cs
@@ -14,7 +14,7 @@ public partial class ToDoListAdminPages : PageTest
         var faker = new Faker();
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
-        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("toDoListNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("To Do List Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("To Do List Add");
