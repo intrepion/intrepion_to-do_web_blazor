c7098395f9135eda172aad94373cef96a221e1b3
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-10 20:32:34 -0700

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder/* Intrepion.ToDo/

--- Intrepion.ToDo/Components/Account/IdentityNoOpEmailSender.cs
+++ Intrepion.ToDo/Components/Account/IdentityNoOpEmailSender.cs
@@ -1,8 +1,8 @@
-﻿using Intrepion.ToDo.Data;
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 
-namespace Intrepion.ToDo.Components.Account;
+namespace ApplicationNamePlaceholder.Components.Account;
 
 // Remove the "else if (EmailSender is IdentityNoOpEmailSender)" block from RegisterConfirmation.razor after updating with a real implementation.
 internal sealed class IdentityNoOpEmailSender : IEmailSender<ApplicationUser>
