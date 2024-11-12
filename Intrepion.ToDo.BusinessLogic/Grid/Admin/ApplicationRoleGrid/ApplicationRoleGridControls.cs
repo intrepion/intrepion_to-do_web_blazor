bc9790697977432e8b51c75198cdde6a495523f8
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 12:53:46 -0700

    dotnet format

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ApplicationRoleGrid/ApplicationRoleGridControls.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ApplicationRoleGrid/ApplicationRoleGridControls.cs
@@ -1,4 +1,4 @@
-namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.ApplicationRoleGrid;
+﻿namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.ApplicationRoleGrid;
 
 // State of grid filters.
 public class ApplicationRoleGridControls(IPageHelper pageHelper) : IApplicationRoleFilters
