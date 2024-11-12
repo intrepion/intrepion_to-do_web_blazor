2ab68eb4ac15cb3a66754af7f532374d99e152a5
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 23:49:33 -0700

    dotnet format

--- Intrepion.ToDo.Shared/ExampleJsInterop.cs
+++ Intrepion.ToDo.Shared/ExampleJsInterop.cs
@@ -1,4 +1,4 @@
-using Microsoft.JSInterop;
+﻿using Microsoft.JSInterop;
 
 namespace Intrepion.ToDo.Shared;
 
@@ -15,7 +15,7 @@ public class ExampleJsInterop : IAsyncDisposable
 
     public ExampleJsInterop(IJSRuntime jsRuntime)
     {
-        moduleTask = new (() => jsRuntime.InvokeAsync<IJSObjectReference>(
+        moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
             "import", "./_content/Intrepion.ToDo.Shared/exampleJsInterop.js").AsTask());
     }
 
