7caaea642da593b08f72ce0da2f3e77b7fe4dace
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 23:55:41 -0700

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder.BusinessLogic/* Intrepion.ToDo.BusinessLogic/

---
+++ Intrepion.ToDo.BusinessLogic/Entities/Etcs/ApplicationUserLoginEtc.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
+
+public class ApplicationUserLoginEtc : IEntityTypeConfiguration<ApplicationUserLogin>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUserLogin> builder)
+    {
+        builder.ToTable("AspNetUserLogins", x => x.IsTemporal());
+
+        builder.HasOne(x => x.ApplicationUserUpdatedBy)
+            .WithMany(x => x.UpdatedApplicationUserLogins)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
