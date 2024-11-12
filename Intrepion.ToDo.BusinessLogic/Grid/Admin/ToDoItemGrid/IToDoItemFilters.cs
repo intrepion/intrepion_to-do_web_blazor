5629607e5c6e70eb27ba813c59a2cbb6078ac941
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 12:50:52 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"ToDoItem\") }; { print }" Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/IToDoItemFilters.cs

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/IToDoItemFilters.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/IToDoItemFilters.cs
@@ -1,10 +1,10 @@
-﻿namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.ToDoItemGrid;
 
 // Interface for filtering.
-public interface IEntityNamePlaceholderFilters
+public interface IToDoItemFilters
 {
-    // The EntityNamePlaceholderFilterColumns being filtered on.
-    EntityNamePlaceholderFilterColumns FilterColumn { get; set; }
+    // The ToDoItemFilterColumns being filtered on.
+    ToDoItemFilterColumns FilterColumn { get; set; }
 
     // Loading indicator.
     bool Loading { get; set; }
@@ -18,6 +18,6 @@ public interface IEntityNamePlaceholderFilters
     // Gets or sets a value indicating if the sort is ascending or descending.
     bool SortAscending { get; set; }
 
-    // The EntityNamePlaceholderFilterColumns being sorted.
-    EntityNamePlaceholderFilterColumns SortColumn { get; set; }
+    // The ToDoItemFilterColumns being sorted.
+    ToDoItemFilterColumns SortColumn { get; set; }
 }
