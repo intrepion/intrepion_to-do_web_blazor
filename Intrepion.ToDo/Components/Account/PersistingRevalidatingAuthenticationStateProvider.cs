6b9d639d5af5ccd58b2f7c2ab085c73fae8729c9
Oliver Forral <intrepion@gmail.com>
2024-11-18 14:49:02 -0700

    dotnet format

--- Intrepion.ToDo/Components/Account/PersistingRevalidatingAuthenticationStateProvider.cs
+++ Intrepion.ToDo/Components/Account/PersistingRevalidatingAuthenticationStateProvider.cs
@@ -1,13 +1,13 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Security.Claims;
+using Intrepion.ToDo.Client;
+using Intrepion.ToDo.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
-using Intrepion.ToDo.Client;
-using Intrepion.ToDo.Data;
 
 namespace Intrepion.ToDo.Components.Account;
 
