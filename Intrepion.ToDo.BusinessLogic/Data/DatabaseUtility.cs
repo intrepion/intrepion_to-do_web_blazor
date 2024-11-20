c90f07c6b5fcf59a4a9e5b44b4e202d42ad0bc88
Oliver Forral <intrepion@gmail.com>
2024-11-20 15:40:32 -0700

    gawk -i inplace "{ gsub(/\/\/ ImporterSecondCodePlaceholder/, \"await ToDoItemImporter.ImportAsync(applicationDbContext, adminUserPass, toDoItemCsvFilePath);\n\ \ \ \ \ \ \ \ // ImporterSecondCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Data/DatabaseUtility.cs

--- Intrepion.ToDo.BusinessLogic/Data/DatabaseUtility.cs
+++ Intrepion.ToDo.BusinessLogic/Data/DatabaseUtility.cs
@@ -80,6 +80,7 @@ public static class DatabaseUtility
         await ApplicationUserRoleImporter.ImportAsync(applicationDbContext, adminUserPass, applicationUserRoleCsvFilePath);
         await ApplicationUserImporter.ImportAsync(applicationDbContext, adminUserPass, applicationUserCsvFilePath);
 
+        await ToDoItemImporter.ImportAsync(applicationDbContext, adminUserPass, toDoItemCsvFilePath);
         // ImporterSecondCodePlaceholder
     }
 }
