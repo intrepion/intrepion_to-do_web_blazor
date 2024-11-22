c3e74cffaaaf5cdee083df999190e2aa4b356397
Oliver Forral <intrepion@gmail.com>
2024-11-21 20:29:53 -0700

    gawk -i inplace "{ gsub(/\/\/ NewEntityCodePlaceholder/, \"CreateDateTime = record.CreateDateTime,\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // NewEntityCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoListImporter.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoListImporter.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoListImporter.cs
@@ -58,6 +58,7 @@ public static class ToDoListImporter
                 {
                     ApplicationUserUpdatedBy = applicationUserUpdatedBy,
 
+                    CreateDateTime = record.CreateDateTime,
                     // NewEntityCodePlaceholder
                 };
 
