9cb21e1d278fd2d3d6aeec57a4e9348b96ee8551
Oliver Forral <intrepion@gmail.com>
2024-11-21 20:26:40 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public DateTime CreateDateTime { get; set; }\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs

--- Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs
@@ -7,5 +7,6 @@ public class ToDoItem
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
 
+    public DateTime CreateDateTime { get; set; }
     // ActualPropertyPlaceholder
 }
