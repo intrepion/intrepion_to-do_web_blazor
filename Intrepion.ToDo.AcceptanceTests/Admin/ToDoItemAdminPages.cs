661d129d4944413c49d4e9dd4659369e37330dcd
Oliver Forral <intrepion@gmail.com>
2024-11-22 22:20:02 -0700

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.AcceptanceTests/Admin/EntityNamePlaceholderAdminPages.cs Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs

---
+++ Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
@@ -0,0 +1,61 @@
+﻿using Bogus;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace ApplicationNamePlaceholder.AcceptanceTests.Admin;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public partial class EntityNamePlaceholderAdminPages : PageTest
+{
+    [Test]
+    public async Task MainNavigation()
+    {
+        var faker = new Faker();
+        var aRandomString = faker.Random.String2(10);
+        var someRandomString = faker.Random.String2(10);
+        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Home");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Add");
+
+        // CreatePropertyCodePlaceholder
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder View");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Edit" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Edit");
+
+        // ModifyPropertyCodePlaceholder
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder View");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Delete" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Confirm" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Back to Grid" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Home");
+    }
+
+    [SetUp]
+    public async Task SetUp()
+    {
+        var baseUrl = Environment.GetEnvironmentVariable("BASE_URL");
+        if (string.IsNullOrEmpty(baseUrl))
+        {
+            baseUrl = "ClientUriPlaceholder";
+        }
+        await Page.GotoAsync(baseUrl);
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Log in");
+        await Page.GetByTestId("loginEmail").FillAsync("Admin1@ApplicationNamePlaceholder.com");
+        await Page.GetByTestId("loginPassword").FillAsync("Admin1@ApplicationNamePlaceholder.com");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Logout" }).ClickAsync();
+    }
+}
