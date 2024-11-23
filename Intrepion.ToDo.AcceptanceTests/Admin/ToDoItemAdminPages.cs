bb756739ff66bf6ffff77ffc76910a7208d2e57a
Oliver Forral <intrepion@gmail.com>
2024-11-22 22:20:03 -0700

    gawk -i inplace "{ gsub(/HumanNamePlaceholder/, \"To Do Item\") }; { print }" Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs

--- Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
+++ Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
@@ -15,25 +15,25 @@ public partial class ToDoItemAdminPages : PageTest
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
         await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Home");
+        await Expect(Page).ToHaveTitleAsync("To Do Item Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Add");
+        await Expect(Page).ToHaveTitleAsync("To Do Item Add");
 
         // CreatePropertyCodePlaceholder
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder View");
+        await Expect(Page).ToHaveTitleAsync("To Do Item View");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Edit" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Edit");
+        await Expect(Page).ToHaveTitleAsync("To Do Item Edit");
 
         // ModifyPropertyCodePlaceholder
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder View");
+        await Expect(Page).ToHaveTitleAsync("To Do Item View");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Delete" }).ClickAsync();
         await Page.GetByRole(AriaRole.Button, new() { Name = "Confirm" }).ClickAsync();
         await Page.GetByRole(AriaRole.Link, new() { Name = "Back to Grid" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Home");
+        await Expect(Page).ToHaveTitleAsync("To Do Item Home");
     }
 
     [SetUp]
