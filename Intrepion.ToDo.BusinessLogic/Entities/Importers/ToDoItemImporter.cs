9829a24ea1215d19cd6a4ebeb271b6bb496d9d06
Oliver Forral <intrepion@gmail.com>
2024-11-25 15:38:56 -0700

    gawk -i inplace "{ gsub(/\/\/ ManyToOneCodePlaceholder/, \");\n\n\ \ \ \ \ \ \ \ \ \ \ \ // ManyToOneCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs
@@ -52,6 +52,8 @@ public static class ToDoItemImporter
             var applicationUser = applicationUserList.FirstOrDefault(x =>
                 true
                 && x.NormalizedUserName.Equals(record.ApplicationUser_NormalizedUserName)
+            );
+
             // ManyToOneCodePlaceholder
 
             if (true
