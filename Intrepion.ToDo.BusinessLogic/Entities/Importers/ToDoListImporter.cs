3995f93e900a7093eca368e1b5b15489d1ae31c3
Oliver Forral <intrepion@gmail.com>
2024-11-25 15:41:00 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"ToDoList\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoListImporter.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoListImporter.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoListImporter.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Importers;
 
-public static class EntityNamePlaceholderImporter
+public static class ToDoListImporter
 {
     public static async Task ImportAsync(
        ApplicationDbContext context,
@@ -42,7 +42,7 @@ public static class EntityNamePlaceholderImporter
             Delimiter = "|",
         });
 
-        var records = csv.GetRecords<EntityNamePlaceholderRecord>();
+        var records = csv.GetRecords<ToDoListRecord>();
 
         // EntityListCodePlaceholder
 
@@ -54,25 +54,25 @@ public static class EntityNamePlaceholderImporter
                 // NullCheckCodePlaceholder
             )
             {
-                var LowercaseNamePlaceholder = new EntityNamePlaceholder
+                var LowercaseNamePlaceholder = new ToDoList
                 {
                     ApplicationUserUpdatedBy = applicationUserUpdatedBy,
 
                     // NewEntityCodePlaceholder
                 };
 
-                var dbEntityNamePlaceholder = await context.TableNamePlaceholder.SingleOrDefaultAsync(
+                var dbToDoList = await context.TableNamePlaceholder.SingleOrDefaultAsync(
                     x => true
                     // CompositeKeyCodePlaceholder
                 );
 
-                if (dbEntityNamePlaceholder is null)
+                if (dbToDoList is null)
                 {
                     await context.TableNamePlaceholder.AddAsync(LowercaseNamePlaceholder);
                 }
                 else
                 {
-                    dbEntityNamePlaceholder.ApplicationUserUpdatedBy = applicationUserUpdatedBy;
+                    dbToDoList.ApplicationUserUpdatedBy = applicationUserUpdatedBy;
 
                     // ExistingEntityCodePlaceholder
                 }
