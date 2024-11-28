7b43ab25ae7e51a3b1bc3b18963aff71917ff6aa
Oliver Forral <intrepion@gmail.com>
2024-11-27 23:59:38 -0700

    gawk -i inplace "{ gsub(/\/\/ ModifyPropertyCodePlaceholder/, \"await Page.GetByLabel(\\\"Is Completed?:\\\", new() { Exact = true }).CheckAsync();\n\ \ \ \ \ \ \ \ // ModifyPropertyCodePlaceholder\") }; { print }" Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs

--- Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
+++ Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
@@ -29,6 +29,7 @@ public partial class ToDoItemAdminPages : PageTest
         await Expect(Page).ToHaveTitleAsync("To Do Item Edit");
 
         await Page.GetByLabel("Due Date Time:", new() { Exact = true }).FillAsync("2022-02-02");
+        await Page.GetByLabel("Is Completed?:", new() { Exact = true }).CheckAsync();
         // ModifyPropertyCodePlaceholder
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
