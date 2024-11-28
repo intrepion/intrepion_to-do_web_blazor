57b043344e604b72614154a59fc56b0b9c420f40
Oliver Forral <intrepion@gmail.com>
2024-11-27 21:00:44 -0700

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/Etcs/EntityNamePlaceholderEtc.cs Intrepion.ToDo.BusinessLogic/Entities/Etcs/ToDoListEtc.cs

---
+++ Intrepion.ToDo.BusinessLogic/Entities/Etcs/ToDoListEtc.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
+
+public class EntityNamePlaceholderEtc : IEntityTypeConfiguration<EntityNamePlaceholder>
+{
+    public void Configure(EntityTypeBuilder<EntityNamePlaceholder> builder)
+    {
+        builder.HasOne(x => x.ApplicationUserUpdatedBy)
+            .WithMany(x => x.UpdatedTableNamePlaceholder)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // EntityConfigurationCodePlaceholder
+    }
+}
