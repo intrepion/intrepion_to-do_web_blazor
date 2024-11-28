d207f853e10571251b2b995844922ec6959d89a2
Oliver Forral <intrepion@gmail.com>
2024-11-28 16:11:12 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.ToDo\") }; { print }" Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/IToDoItemFilters.cs

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/IToDoItemFilters.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/IToDoItemFilters.cs
@@ -1,4 +1,4 @@
-﻿namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
 
 public interface IEntityNamePlaceholderFilters
 {
