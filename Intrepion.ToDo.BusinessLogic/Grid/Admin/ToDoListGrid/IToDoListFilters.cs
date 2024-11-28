067eb4d193a14a583252bd0f401970881f1c070b
Oliver Forral <intrepion@gmail.com>
2024-11-28 16:13:20 -0700

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Grid/Admin/EntityNamePlaceholderGrid/IEntityNamePlaceholderFilters.cs Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoListGrid/IToDoListFilters.cs

---
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoListGrid/IToDoListFilters.cs
@@ -0,0 +1,16 @@
+﻿namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+
+public interface IEntityNamePlaceholderFilters
+{
+    EntityNamePlaceholderFilterColumns FilterColumn { get; set; }
+
+    bool Loading { get; set; }
+
+    string? FilterText { get; set; }
+
+    IPageHelper PageHelper { get; set; }
+
+    bool SortAscending { get; set; }
+
+    EntityNamePlaceholderFilterColumns SortColumn { get; set; }
+}
