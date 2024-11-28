18b8aa3f78a6b9959420c911765b85d93f361ec3
Oliver Forral <intrepion@gmail.com>
2024-11-27 20:58:52 -0700

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/EntityNamePlaceholder.cs Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs

---
+++ Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
+
+public class EntityNamePlaceholder
+{
+    public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
+    public Guid Id { get; set; }
+    public DateTime CreateDateTime { get; set; }
+    public DateTime UpdateDateTime { get; set; }
+
+    // ActualPropertyPlaceholder
+}
