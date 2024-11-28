639d2a130e8f6b83608f23a5e0524fb4b4c24ef1
Oliver Forral <intrepion@gmail.com>
2024-11-28 00:01:53 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.ToDo\") }; { print }" Intrepion.ToDo.AcceptanceTests/Admin/*

--- Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
+++ Intrepion.ToDo.AcceptanceTests/Admin/ToDoItemAdminPages.cs
@@ -2,7 +2,7 @@
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
-namespace ApplicationNamePlaceholder.AcceptanceTests.Admin;
+namespace Intrepion.ToDo.AcceptanceTests.Admin;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
@@ -56,8 +56,8 @@ public partial class ToDoItemAdminPages : PageTest
 
         await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Log in");
-        await Page.GetByTestId("loginEmail").FillAsync("Admin1@ApplicationNamePlaceholder.com");
-        await Page.GetByTestId("loginPassword").FillAsync("Admin1@ApplicationNamePlaceholder.com");
+        await Page.GetByTestId("loginEmail").FillAsync("Admin1@Intrepion.ToDo.com");
+        await Page.GetByTestId("loginPassword").FillAsync("Admin1@Intrepion.ToDo.com");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
     }
 
