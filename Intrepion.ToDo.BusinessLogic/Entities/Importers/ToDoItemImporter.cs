151a12c2e1b2fe544a6b867d8498282aa1b11d70
Oliver Forral <intrepion@gmail.com>
2024-11-28 16:10:59 -0700

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/Importers/EntityNamePlaceholderImporter.cs Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs

---
+++ Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs
@@ -0,0 +1,84 @@
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using ApplicationNamePlaceholder.BusinessLogic.Data;
+using ApplicationNamePlaceholder.BusinessLogic.Entities.Records;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Importers;
+
+public static class EntityNamePlaceholderImporter
+{
+    public static async Task ImportAsync(
+       ApplicationDbContext context,
+       string userName, string csvPath
+    )
+    {
+        if (!File.Exists(csvPath))
+        {
+            Console.WriteLine("File not found: " + csvPath);
+            return;
+        }
+
+        if (context.TableNamePlaceholder is null)
+        {
+            Console.WriteLine("Database table not found: context.TableNamePlaceholder");
+            return;
+        }
+
+        var normalizedUserName = userName.ToUpperInvariant();
+        var applicationUserUpdatedBy = await context.Users.SingleOrDefaultAsync(x => x.NormalizedUserName != null && x.NormalizedUserName.Equals(normalizedUserName));
+
+        if (applicationUserUpdatedBy is null)
+        {
+            Console.WriteLine("UserName not found: " + userName);
+            return;
+        }
+
+        using var reader = new StreamReader(csvPath);
+        using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            PrepareHeaderForMatch = x => x.Header.ToUpper(CultureInfo.InvariantCulture),
+            Delimiter = "|",
+        });
+
+        var records = csv.GetRecords<EntityNamePlaceholderRecord>();
+
+        // EntityListCodePlaceholder
+
+        foreach (var record in records)
+        {
+            // ManyToOneCodePlaceholder
+
+            if (true
+                // NullCheckCodePlaceholder
+            )
+            {
+                var LowercaseNamePlaceholder = new EntityNamePlaceholder
+                {
+                    ApplicationUserUpdatedBy = applicationUserUpdatedBy,
+
+                    // NewEntityCodePlaceholder
+                };
+
+                var dbEntityNamePlaceholder = await context.TableNamePlaceholder.SingleOrDefaultAsync(
+                    x => true
+                    // CompositeKeyCodePlaceholder
+                );
+
+                if (dbEntityNamePlaceholder is null)
+                {
+                    await context.TableNamePlaceholder.AddAsync(LowercaseNamePlaceholder);
+                }
+                else
+                {
+                    dbEntityNamePlaceholder.ApplicationUserUpdatedBy = applicationUserUpdatedBy;
+
+                    // ExistingEntityCodePlaceholder
+                }
+            }
+        }
+
+        await context.SaveChangesAsync();
+    }
+}
