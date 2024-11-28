70a8d28ce5f9118d18b2600bd3e2500b961d28b8
Oliver Forral <intrepion@gmail.com>
2024-11-27 23:59:11 -0700

    gawk -i inplace "{ gsub(/\/\/ NullCheckCodePlaceholder/, \"\\\\&\\\\& toDoList is not null\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // NullCheckCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs
@@ -53,6 +53,7 @@ public static class ToDoItemImporter
             if (true
                 && applicationUser is not null
                 && toDoList is not null
+                && toDoList is not null
                 // NullCheckCodePlaceholder
             )
             {
