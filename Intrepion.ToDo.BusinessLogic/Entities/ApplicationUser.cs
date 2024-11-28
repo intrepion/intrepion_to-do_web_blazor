597cb13cf1aefa40f0d26f7809bd4451437e6ea2
Oliver Forral <intrepion@gmail.com>
2024-11-28 16:14:18 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.ToDo\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/*

--- Intrepion.ToDo.BusinessLogic/Entities/ApplicationUser.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/ApplicationUser.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
+namespace Intrepion.ToDo.BusinessLogic.Entities;
 
 public class ApplicationUser : IdentityUser<Guid>
 {
