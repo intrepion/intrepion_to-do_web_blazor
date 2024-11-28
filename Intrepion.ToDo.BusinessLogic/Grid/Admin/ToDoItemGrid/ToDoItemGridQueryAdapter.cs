c3b2527f7723fc67911668da8230a63152711f52
Oliver Forral <intrepion@gmail.com>
2024-11-28 16:11:47 -0700

    gawk -i inplace "{ gsub(/\/\/ SortExpressionCodePlaceholder/, \"{ ToDoItemFilterColumns.DueDateTime, x => x != null ? x.DueDateTime.ToString() : string.Empty },\n\ \ \ \ \ \ \ \ \ \ \ \ // SortExpressionCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/ToDoItemGridQueryAdapter.cs

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/ToDoItemGridQueryAdapter.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoItemGrid/ToDoItemGridQueryAdapter.cs
@@ -14,6 +14,7 @@ public class ToDoItemGridQueryAdapter
         {
             { ToDoItemFilterColumns.Id, x => !x.Id.Equals(Guid.Empty) ? x.Id.ToString() : string.Empty },
 
+            { ToDoItemFilterColumns.DueDateTime, x => x != null ? x.DueDateTime.ToString() : string.Empty },
             // SortExpressionCodePlaceholder
         };
 
