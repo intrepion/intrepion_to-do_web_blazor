b7b0dbe1689ad4c14eceafd0264f7c72d92bf411
Oliver Forral <intrepion@gmail.com>
2024-11-27 20:58:20 -0700

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder.BusinessLogic/* Intrepion.ToDo.BusinessLogic/

---
+++ Intrepion.ToDo.BusinessLogic/Entities/Etcs/ApplicationRoleClaimEtc.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
+
+public class ApplicationRoleClaimEtc : IEntityTypeConfiguration<ApplicationRoleClaim>
+{
+    public void Configure(EntityTypeBuilder<ApplicationRoleClaim> builder)
+    {
+        builder.HasOne(x => x.ApplicationUserUpdatedBy)
+            .WithMany(x => x.UpdatedApplicationRoleClaims)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
