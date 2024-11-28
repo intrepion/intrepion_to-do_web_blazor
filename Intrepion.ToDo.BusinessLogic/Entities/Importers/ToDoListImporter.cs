3d76b92f474c07eb9f5fb1aa325711db3c607318
Oliver Forral <intrepion@gmail.com>
2024-11-28 16:13:32 -0700

    gawk -i inplace "{ gsub(/\/\/ ManyToOneCodePlaceholder/, \"var applicationUser = applicationUserList.FirstOrDefault(x =>\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ true\n\ \ \ \ \ \ \ \ \ \ \ \ // ManyToOneCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoListImporter.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoListImporter.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoListImporter.cs
@@ -49,6 +49,8 @@ public static class ToDoListImporter
 
         foreach (var record in records)
         {
+            var applicationUser = applicationUserList.FirstOrDefault(x =>
+                true
             // ManyToOneCodePlaceholder
 
             if (true
