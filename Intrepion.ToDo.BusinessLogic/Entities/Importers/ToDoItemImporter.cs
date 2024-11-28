206bb5d7811188fc945b92a0f6f79a9cef5ec019
Oliver Forral <intrepion@gmail.com>
2024-11-28 16:12:29 -0700

    gawk -i inplace "{ gsub(/\/\/ NewEntityCodePlaceholder/, \"Title = record.Title,\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // NewEntityCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs
@@ -81,6 +81,7 @@ public static class ToDoItemImporter
                     IsCompleted = record.IsCompleted,
                     Ordering = record.Ordering,
                     ToDoList = toDoList,
+                    Title = record.Title,
                     // NewEntityCodePlaceholder
                 };
 
