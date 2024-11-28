5cdf354eea3e297f2a229576b51db14e467a6829
Oliver Forral <intrepion@gmail.com>
2024-11-28 16:10:59 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"ToDoItem\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs

--- Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs
@@ -2,7 +2,7 @@
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
 
-public class EntityNamePlaceholder
+public class ToDoItem
 {
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
