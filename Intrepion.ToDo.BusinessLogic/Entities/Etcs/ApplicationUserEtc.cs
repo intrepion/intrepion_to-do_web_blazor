012e71eddbb594cfec92398ae6d3b5f0c3b5339f
Oliver Forral <intrepion@gmail.com>
2024-11-28 16:13:37 -0700

    gawk -i inplace "{ gsub(/\/\/ EntityConfigurationCodePlaceholder/, \"builder.HasMany(x => x.ToDoLists)\n\ \ \ \ \ \ \ \ \ \ \ \ .WithOne(x => x.ApplicationUser)\n\ \ \ \ \ \ \ \ \ \ \ \ .OnDelete(DeleteBehavior.Restrict);\n\ \ \ \ \ \ \ \ // EntityConfigurationCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Etcs/ApplicationUserEtc.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Etcs/ApplicationUserEtc.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Etcs/ApplicationUserEtc.cs
@@ -14,6 +14,9 @@ public class ApplicationUserEtc : IEntityTypeConfiguration<ApplicationUser>
         builder.HasMany(x => x.ToDoItems)
             .WithOne(x => x.ApplicationUser)
             .OnDelete(DeleteBehavior.Restrict);
+        builder.HasMany(x => x.ToDoLists)
+            .WithOne(x => x.ApplicationUser)
+            .OnDelete(DeleteBehavior.Restrict);
         // EntityConfigurationCodePlaceholder
     }
 }
