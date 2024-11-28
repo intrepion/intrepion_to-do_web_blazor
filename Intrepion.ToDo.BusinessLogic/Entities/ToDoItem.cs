2d4bf24200137735c1bc8c85fd51b27b128d91d4
Oliver Forral <intrepion@gmail.com>
2024-11-28 00:00:08 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"[Required]\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs

--- Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/ToDoItem.cs
@@ -14,5 +14,6 @@ public class ToDoItem
     public bool IsCompleted { get; set; }
     public int Ordering { get; set; }
     public ToDoList? ToDoList { get; set; }
+    [Required]
     // ActualPropertyPlaceholder
 }
