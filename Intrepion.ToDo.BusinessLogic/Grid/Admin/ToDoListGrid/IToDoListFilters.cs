d77b62b621243511b6d7b58f3b22a5e4bfef1493
Oliver Forral <intrepion@gmail.com>
2024-11-28 16:13:21 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"ToDoList\") }; { print }" Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoListGrid/IToDoListFilters.cs

--- Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoListGrid/IToDoListFilters.cs
+++ Intrepion.ToDo.BusinessLogic/Grid/Admin/ToDoListGrid/IToDoListFilters.cs
@@ -1,8 +1,8 @@
-﻿namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace Intrepion.ToDo.BusinessLogic.Grid.Admin.ToDoListGrid;
 
-public interface IEntityNamePlaceholderFilters
+public interface IToDoListFilters
 {
-    EntityNamePlaceholderFilterColumns FilterColumn { get; set; }
+    ToDoListFilterColumns FilterColumn { get; set; }
 
     bool Loading { get; set; }
 
@@ -12,5 +12,5 @@ public interface IEntityNamePlaceholderFilters
 
     bool SortAscending { get; set; }
 
-    EntityNamePlaceholderFilterColumns SortColumn { get; set; }
+    ToDoListFilterColumns SortColumn { get; set; }
 }
