4f4d6b4d84690b2699bbead0d25b5134d3ac5d7b
Oliver Forral <intrepion@gmail.com>
2024-11-27 20:59:22 -0700

    gawk -i inplace "{ gsub(/\/\/ ExistingEntityCodePlaceholder/, \"dbToDoItem.ApplicationUser = applicationUser;\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // ExistingEntityCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Importers/ToDoItemImporter.cs
@@ -87,6 +87,7 @@ public static class ToDoItemImporter
                 {
                     dbToDoItem.ApplicationUserUpdatedBy = applicationUserUpdatedBy;
 
+                    dbToDoItem.ApplicationUser = applicationUser;
                     // ExistingEntityCodePlaceholder
                 }
             }
