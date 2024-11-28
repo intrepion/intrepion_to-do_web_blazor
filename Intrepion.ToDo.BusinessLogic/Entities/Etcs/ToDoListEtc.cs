5e9a57838053bd85f2109ebaf30ecc50b0024464
Oliver Forral <intrepion@gmail.com>
2024-11-28 00:00:54 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"ToDoList\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Etcs/ToDoListEtc.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Etcs/ToDoListEtc.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Etcs/ToDoListEtc.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
 
-public class EntityNamePlaceholderEtc : IEntityTypeConfiguration<EntityNamePlaceholder>
+public class ToDoListEtc : IEntityTypeConfiguration<ToDoList>
 {
-    public void Configure(EntityTypeBuilder<EntityNamePlaceholder> builder)
+    public void Configure(EntityTypeBuilder<ToDoList> builder)
     {
         builder.HasOne(x => x.ApplicationUserUpdatedBy)
             .WithMany(x => x.UpdatedTableNamePlaceholder)
