34cac7340249cb55cc00740114986a028dd19751
Oliver Forral <intrepion@gmail.com>
2024-11-27 21:01:18 -0700

    gawk -i inplace "{ gsub(/\/\/ ModifyPropertyCodePlaceholder/, \"await Page.GetByLabel(\\\"Title:\\\", new() { Exact = true }).FillAsync(\\\"someTitle\\\" + someRandomString);\n\ \ \ \ \ \ \ \ // ModifyPropertyCodePlaceholder\") }; { print }" Intrepion.ToDo.AcceptanceTests/Admin/ToDoListAdminPages.cs

--- Intrepion.ToDo.AcceptanceTests/Admin/ToDoListAdminPages.cs
+++ Intrepion.ToDo.AcceptanceTests/Admin/ToDoListAdminPages.cs
@@ -27,6 +27,7 @@ public partial class ToDoListAdminPages : PageTest
         await Page.GetByRole(AriaRole.Link, new() { Name = "Edit" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("To Do List Edit");
 
+        await Page.GetByLabel("Title:", new() { Exact = true }).FillAsync("someTitle" + someRandomString);
         // ModifyPropertyCodePlaceholder
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
