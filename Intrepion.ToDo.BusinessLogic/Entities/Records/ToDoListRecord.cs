8d97d386b88429a1c09208ce231e1ea483e0e041
Oliver Forral <intrepion@gmail.com>
2024-11-27 21:01:20 -0700

    gawk -i inplace "{ gsub(/\/\/ RecordPropertyCodePlaceholder/, \"public string Title { get; set; } = string.Empty;\n\ \ \ \ // RecordPropertyCodePlaceholder\") }; { print }" Intrepion.ToDo.BusinessLogic/Entities/Records/ToDoListRecord.cs

--- Intrepion.ToDo.BusinessLogic/Entities/Records/ToDoListRecord.cs
+++ Intrepion.ToDo.BusinessLogic/Entities/Records/ToDoListRecord.cs
@@ -3,5 +3,6 @@
 public class ToDoListRecord
 {
     public string ApplicationUser_NormalizedUserName { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
     // RecordPropertyCodePlaceholder
 }
