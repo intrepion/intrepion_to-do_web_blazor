ea05adc28b0fc58f89bb0674182618b71a913023
Oliver Forral <intrepion@gmail.com>
2024-11-28 16:14:09 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.ToDo\") }; { print }" Intrepion.ToDo/Components/Account/*

--- Intrepion.ToDo/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ Intrepion.ToDo/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -1,8 +1,8 @@
 ﻿using System.Security.Claims;
 using System.Text.Json;
-using ApplicationNamePlaceholder.BusinessLogic.Entities;
-using ApplicationNamePlaceholder.Components.Account.Pages;
-using ApplicationNamePlaceholder.Components.Account.Pages.Manage;
+using Intrepion.ToDo.BusinessLogic.Entities;
+using Intrepion.ToDo.Components.Account.Pages;
+using Intrepion.ToDo.Components.Account.Pages.Manage;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Http.Extensions;
