8a564ec05bd5b25ffd3920e21b3d8ad4eb8df8df
Oliver Forral <intrepion@gmail.com>
2025-04-03 15:15:13 -0700

    dotnet format

---
+++ Intrepion.ToDo.Client.BlazorTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Intrepion.ToDo.Client.BlazorTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
