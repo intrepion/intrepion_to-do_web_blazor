bdafec4063ae7b0291e74981c9104faafdb064b5
Oliver Forral <intrepion@gmail.com>
2025-04-26 16:55:25 -0600

    dotnet ef --project Intrepion.ToDo.BusinessLogic --startup-project Intrepion.ToDo migrations add Initial

---
+++ Intrepion.ToDo.BusinessLogic/Entities/Records/ToDoItemRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace Intrepion.ToDo.BusinessLogic.Entities.Records;
+
+public class ToDoItemRecord
+{
+    public bool IsCompleted { get; set; }
+    public int Ordering { get; set; }
+    public string ToDoList_NormalizedTitle { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    // RecordPropertyCodePlaceholder
+}
