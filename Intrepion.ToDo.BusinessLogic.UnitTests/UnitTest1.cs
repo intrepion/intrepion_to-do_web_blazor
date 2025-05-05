5425d3d8df1756d2c036994bad6cbd6ce82cc493
Oliver Forral <intrepion@gmail.com>
2025-05-04 21:00:00 -0600

    dotnet format

---
+++ Intrepion.ToDo.BusinessLogic.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Intrepion.ToDo.BusinessLogic.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Intrepion.ToDo.Shared.ComponentTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Intrepion.ToDo.Shared.ComponentTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
